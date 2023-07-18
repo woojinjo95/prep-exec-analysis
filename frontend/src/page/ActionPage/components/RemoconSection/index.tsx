6584137ef8216b00ffc6455b7430de56f488d6cb
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-07-18 13:50:29 +0900

    Remocon section layout 잡기 + resize에 따른 remocon 크기 설정

--- frontend/src/page/ActionPage/components/RemoconSection/index.tsx
+++ frontend/src/page/ActionPage/components/RemoconSection/index.tsx
@@ -1,10 +1,57 @@
-import React from 'react'
+import React, { useEffect, useRef, useState } from 'react'
+import Remocon from '@assets/images/btv_remote_control.png'
 
 /**
  * 리모컨 영역
  */
 const RemoconSection: React.FC = () => {
-  return <section className="border border-black">remocon</section>
+  const remoconDivRef = useRef<HTMLDivElement>(null)
+
+  const [remoconHeight, setRemoconHeight] = useState<number>(0)
+
+  useEffect(() => {
+    const updateRemoconHeight = () => {
+      if (remoconDivRef.current) {
+        setRemoconHeight(remoconDivRef.current.offsetHeight)
+      }
+    }
+
+    updateRemoconHeight()
+
+    const resizeHandler = () => {
+      updateRemoconHeight()
+    }
+
+    window.addEventListener('resize', resizeHandler)
+    return () => {
+      window.removeEventListener('resize', resizeHandler)
+    }
+  }, [remoconDivRef, remoconHeight])
+
+  return (
+    <section className="border border-black h-full">
+      <div className="flex flex-col w-full h-full p-[10px]">
+        <div className="flex flex-row justify-between w-full h-[30px]">
+          <p>dropdown</p>
+          <p>help</p>
+        </div>
+        <div className="h-[calc(100%-30px)] flex flex-row">
+          <div ref={remoconDivRef} className="w-[50%] h-full flex items-center justify-center">
+            <img src={Remocon} alt="remocon" style={{ height: `${remoconHeight * 0.95}px` }} />
+          </div>
+          <div className="w-[50%] flex flex-col">
+            <div className="flex flex-row justify-between">
+              <p>Custom Key</p>
+              <div className="flex flex-row justify-between">
+                <p>+</p>
+                <p>|</p>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default RemoconSection
