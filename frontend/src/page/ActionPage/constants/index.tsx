5be4f5659f0affc25426e3c8183f408c829c512d
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-07-19 15:42:56 +0900

    keyEvent 정의 및 actionPage keydown, keyup event 정의

---
+++ frontend/src/page/ActionPage/constants/index.tsx
@@ -0,0 +1,1 @@
+export type KeyEvent = Pick<KeyboardEvent, 'code' | 'altKey' | 'preventDefault'>
--- frontend/src/page/ActionPage/index.tsx
+++ frontend/src/page/ActionPage/index.tsx
@@ -1,19 +1,45 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { PageContainer } from '@global/ui'
 import ActionSection from './components/ActionSection'
 import MonitorSection from './components/MonitorSection'
 import RemoconSection from './components/RemoconSection'
 import TerminalSection from './components/TerminalSection'
+import { KeyEvent } from './constants'
 
 /**
  * 동작 제어 페이지
  */
 const ActionPage: React.FC = () => {
+  const [event, setEvent] = useState<KeyEvent | null>(null)
+
+  useEffect(() => {
+    const keyDownHandler = (e: KeyEvent) => {
+      if (e.altKey) {
+        setEvent(e)
+        e.preventDefault()
+      }
+    }
+
+    const keyUpHandler = (e: KeyEvent) => {
+      if (!e.altKey) {
+        setEvent(null)
+        e.preventDefault()
+      }
+    }
+
+    window.addEventListener('keydown', keyDownHandler)
+    window.addEventListener('keyup', keyUpHandler)
+    return () => {
+      window.removeEventListener('keydown', keyDownHandler)
+      window.removeEventListener('keyup', keyUpHandler)
+    }
+  }, [])
+
   return (
     <PageContainer className="grid grid-cols-[1.5fr_3fr_1.5fr] grid-rows-[2fr_1.5fr]">
       <ActionSection />
       <MonitorSection />
-      <RemoconSection />
+      <RemoconSection event={event} />
       <TerminalSection />
     </PageContainer>
   )
