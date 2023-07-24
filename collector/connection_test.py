e9566141d87be0b267d1a54b6c1b4f1a043a485f
woojinjo <e05062@nextlab.co.kr>
2023-07-24 19:45:57 +0900

    add connection test code

--- collector/connection_test.py
+++
@@ -1,6 +0,0 @@
-from scripts.log_util.stb_connection.connector import Connection
-from scripts.log_util.stb_connection.utils import exec_command
-
-conn = Connection(host='192.168.30.25', port=5555, username='root', password='', connection_mode='adb')
-result = exec_command('ls', 2, conn)
-print(result)
---
+++ collector/test/connection_test.py
@@ -0,0 +1,13 @@
+from scripts.log_util.stb_connection.connector import Connection
+from scripts.log_util.stb_connection.utils import exec_command
+
+connection_info = {
+    'host': '192.168.30.25',
+    'port': 5555,
+    'username': 'root',
+    'password': '',
+    'connection_mode': 'adb',
+}
+conn = Connection(**connection_info)
+result = exec_command('ls', 2, connection_info)
+print(result)
