6796eabed87804f968609043bb3d39bd8fcd7752
woojinjo <e05062@nextlab.co.kr>
2023-07-25 13:27:34 +0900

    fix test code

--- collector/log_manager_test.py
+++ collector/log_manager_test.py
@@ -1,4 +1,7 @@
-from datetime import datetime
+import time
+from threading import Thread
+import random
+from datetime import datetime, timedelta
 from scripts.file_service.log_manage.log_manager import LogFileManager
 
 
@@ -10,5 +13,39 @@ manager = LogFileManager(connection_info = {
     'connection_mode': 'adb',
 })
 
-manager.save_datas([(datetime.now().timestamp(), 'test1'), (datetime.now().timestamp(), 'test2')])
-print(manager.load_data(datetime.now().timestamp() - 5000, datetime.now().timestamp() + 5000))
+##### Test1 #####
+# start_time = datetime.now()
+# end_time = datetime.now() + timedelta(minutes=1)
+# print(f'start_time: {start_time}, end_time: {end_time}')
+
+# start_timestamp = start_time.timestamp()
+# end_timestamp = end_time.timestamp()
+
+# manager.save_datas([(start_timestamp, 'test1'), (end_timestamp, 'test2')])
+# datas = manager.load_data(start_timestamp, end_timestamp)
+# datas = [(datetime.fromtimestamp(data[0]).strftime('%Y-%m-%d %H:%M:%S'), data[1]) for data in datas]
+# print(datas)
+
+
+##### Test2 #####
+def start_make_data(timeout: float):
+    start_time = time.time()
+    while True:
+        if time.time() > start_time + timeout:
+            break
+        time.sleep(random.random())
+        manager.save_datas([(time.time(), 'test')])
+th = Thread(target=start_make_data(5))
+th.start()
+
+start_time = datetime.now() - timedelta(minutes=1)
+end_time = datetime.now()
+print(f'start_time: {start_time}, end_time: {end_time}')
+start_timestamp = start_time.timestamp()
+end_timestamp = end_time.timestamp()
+
+datas = manager.load_data(start_timestamp, end_timestamp)
+datas = [(datetime.fromtimestamp(data[0]).strftime('%Y-%m-%d %H:%M:%S'), data[1]) for data in datas]
+print(datas)
+
+th.join()
