8ac60713cd5f8c34ccbc38b92c02f45e1603bc40
kjune1236 <kjune1236@gmail.com>
2023-07-26 18:29:52 +0900

    redis connection

--- collector/scripts/file_service/log_manage/log_manager.py
+++ collector/scripts/file_service/log_manage/log_manager.py
@@ -48,9 +48,3 @@ class LogFileManager():
     def stop(self):
         self.__stop_log_collector()
         self.__stop_log_saver()
-
-    # def load_page(self, start: float, end: float, page_number: int=1, page_size: int=1) -> List:
-    #     return self.db_conn.load_data_with_paging(start, end, page_number, page_size)
-
-    # def delete(self, start: float, end: float):
-    #     pass
