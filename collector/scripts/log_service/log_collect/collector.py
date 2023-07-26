39646d9de2830bf2679571f22f5d4bdfb8207519
kjune1236 <kjune1236@gmail.com>
2023-07-26 14:37:28 +0900

    add completed dir in log collector

--- collector/scripts/log_service/log_collect/collector.py
+++ collector/scripts/log_service/log_collect/collector.py
@@ -6,6 +6,7 @@ from typing import List
 from multiprocessing import Event, Queue
 from uuid import uuid4
 import traceback
+import shutil
 
 from iterators import TimeoutIterator
 from scripts.connection.stb_connection.connector import Connection
@@ -22,34 +23,27 @@ def check_stop_events(stop_events) -> bool:
 
 def collect(connection_info: dict, command_script: str, log_type: str, 
             stop_events: List[Event]):
-    # is_running.set()
     stop_event = Event()
     stop_events = (*stop_events, stop_event)
 
-    output_dir = 'logs'
     logging_session_id = str(uuid4())
-    status = 'stopping'
-
     conn = Connection(**connection_info)
     stdout_stop_event = Event()
     stdout = conn.exec_command(command_script, stdout_stop_event)
     timeout_stdout = TimeoutIterator(stdout, timeout=CollectorConfig.LOG_STREAM_TIMEOUT, sentinel=None)
-    directory_path = output_dir
-    os.makedirs(directory_path, exist_ok=True)
+    
+    log_dir = 'logs'
+    os.makedirs(log_dir, exist_ok=True)
+    completed_log_dir = 'completed_logs'
+    os.makedirs(completed_log_dir, exist_ok=True)
 
     log_cell_lines = ""
-    chunk_count = 1
 
     while not check_stop_events(stop_events):
-        if chunk_count == CollectorConfig.SESSION_UPDATE_CHUNK_CNT + 1:
-            chunk_count = 1
-        status = 'running'
-
-        with open(f"{directory_path}/{datetime.now().strftime('%Y%m%d%H%M%S%f')}_{log_type}.log",
+        with open(os.path.join(log_dir, f"{datetime.now().strftime('%Y%m%d%H%M%S%f')}_{log_type}.log"),
                 'w', buffering=1, encoding='utf-8-sig') as f:
             logger.info(f'{f.name} start dump file')
             start_time = time.time()
-            collector_chunk_start_time = datetime.now().isoformat()
 
             for line in timeout_stdout:
                 if check_stop_events(stop_events):
@@ -83,11 +77,6 @@ def collect(connection_info: dict, command_script: str, log_type: str,
                     stop_event.set()
                     break
 
-            if chunk_count == CollectorConfig.SESSION_UPDATE_CHUNK_CNT or check_stop_events(stop_events):
-                is_finish = True
-            else:
-                is_finish = False
-
             # stop event로 종료 할 경우 남은 내용물 다 씀
             # stop event로 종료한게 아니라면, 다음 루프에서 log_cell_lines 내용물들이 다 써지게 되어있음
             if check_stop_events(stop_events) and log_cell_lines != "":
@@ -100,23 +89,14 @@ def collect(connection_info: dict, command_script: str, log_type: str,
 
             file_size = f.tell()
             file_name = f.name
-            if file_size > 0:
-                logger.info(f'{f.name} {logging_session_id} finish dump file / size : {file_size}Byte')
-                # upload_queue.put((f.name, logging_session_id, chunk_count, is_finish, collector_chunk_start_time))
-                chunk_count += 1
-            else:
-                logger.info(f'{f.name} {logging_session_id} skip this file. {file_size}Byte')
-                os.remove(file_name)
-                time.sleep(2)
-                # finish collection & close this connection
+            logger.info(f'{f.name} {logging_session_id} finish dump file / size : {file_size} Byte')
+            shutil.move(file_name, os.path.join(completed_log_dir, os.path.basename(file_name)))
 
             # 먼저 stdout이 비정상적으로 끊어지는 경우가 있다. (콜드부팅 등의 경우에) 이럴땐 그냥 나가기!
             if stdout_stop_event.is_set():
                 break
 
-    status = 'stopping'
     logger.info(f"{logging_session_id} finish collection")
-    # is_running.clear()
 
     # clear stdout and conn
     stdout_stop_event.set()
