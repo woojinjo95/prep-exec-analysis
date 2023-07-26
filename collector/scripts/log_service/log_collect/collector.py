c10df265dd07b874037dba614df3cf5cb7160fcc
kjune1236 <kjune1236@gmail.com>
2023-07-26 11:17:18 +0900

    fix collector from class to def

--- collector/scripts/log_service/log_collect/collector.py
+++ collector/scripts/log_service/log_collect/collector.py
@@ -16,118 +16,112 @@ from .format import CollectorConfig
 logger = logging.getLogger('connection')
 
 
-class Collector:
-    def __init__(self, connection_info: dict, command_script: str, log_type: str, 
-                 upload_queue: Queue, stop_events: List[Event]):
-        self.connection_info = connection_info
-        self.command_script = command_script
-        self.log_type = log_type
-        self.upload_queue = upload_queue
+def check_stop_events(stop_events) -> bool:
+    return any([stop_event.is_set() for stop_event in stop_events if hasattr(stop_event, 'is_set')])
 
-        self.stop_event = Event()
-        self.stop_events = (*stop_events, self.stop_event)
 
-        self.output_dir = 'logs'
-        self.logging_session_id = str(uuid4())
-        self.status = 'stopping'
+def collect(connection_info: dict, command_script: str, log_type: str, 
+                upload_queue: Queue, stop_events: List[Event], is_running: Event):
+    stop_event = Event()
+    stop_events = (*stop_events, stop_event)
 
-    def check_stop_events(self) -> bool:
-        return any([stop_event.is_set() for stop_event in self.stop_events if hasattr(stop_event, 'is_set')])
+    output_dir = 'logs'
+    logging_session_id = str(uuid4())
+    status = 'stopping'
 
-    def collect(self):
-        conn = Connection(**self.connection_info)
-        stdout_stop_event = Event()
-        stdout = conn.exec_command(self.command_script, stdout_stop_event)
-        timeout_stdout = TimeoutIterator(stdout, timeout=CollectorConfig.LOG_STREAM_TIMEOUT, sentinel=None)
-        directory_path = self.output_dir
-        os.makedirs(directory_path, exist_ok=True)
+    conn = Connection(**connection_info)
+    stdout_stop_event = Event()
+    stdout = conn.exec_command(command_script, stdout_stop_event)
+    timeout_stdout = TimeoutIterator(stdout, timeout=CollectorConfig.LOG_STREAM_TIMEOUT, sentinel=None)
+    directory_path = output_dir
+    os.makedirs(directory_path, exist_ok=True)
 
-        log_cell_lines = ""
-        chunk_count = 1
-        
-        while not self.check_stop_events():
-            if chunk_count == CollectorConfig.SESSION_UPDATE_CHUNK_CNT + 1:
-                chunk_count = 1
-            self.status = 'running'
+    log_cell_lines = ""
+    chunk_count = 1
 
-            with open(f"{directory_path}/{datetime.now().strftime('%Y%m%d%H%M%S%f')}_{self.log_type}.log",
-                    'w', buffering=1, encoding='utf-8-sig') as f:
-                logger.info(f'{f.name} start dump file')
-                start_time = time.time()
-                collector_chunk_start_time = datetime.now().isoformat()
+    while not check_stop_events(stop_events):
+        if chunk_count == CollectorConfig.SESSION_UPDATE_CHUNK_CNT + 1:
+            chunk_count = 1
+        status = 'running'
 
-                for line in timeout_stdout:
-                    if self.check_stop_events():
-                        break
-                    # while not stop_event.is_set():
-                    end_condition = [f.tell() >= CollectorConfig.DUMP_FILESIZE_LIMIT,
-                                    time.time() - start_time >= CollectorConfig.DUMP_TIME_LIMIT]
-                    try:
-                        # Timeout 안걸렸을 때
-                        if line is not None:
-                            # 다음 splitter가 등장하면 현재 cell 쓰고 cell 초기화
-                            if log_cell_lines != "" and any([line.startswith(spliter) for spliter in CollectorConfig.LOG_CELL_SPLITER]):
-                                f.write(log_cell_lines)
-                                log_cell_lines = ""
-                            # line 내용물 모음
-                            log_cell_lines += line
-                        # Top의 경우 Splitter가 없기 때문에 Timeout 걸린 직후 다음 cell 기다리는 동안 씀
-                        elif log_cell_lines != "":
-                            if self.log_type == 'top':
-                                # Splitter 강제 주입
-                                log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
-                                    log_cell_lines
-                                f.write(log_cell_lines)
-                                log_cell_lines = ""
+        with open(f"{directory_path}/{datetime.now().strftime('%Y%m%d%H%M%S%f')}_{log_type}.log",
+                'w', buffering=1, encoding='utf-8-sig') as f:
+            logger.info(f'{f.name} start dump file')
+            start_time = time.time()
+            collector_chunk_start_time = datetime.now().isoformat()
 
-                        # end 조건 만족할 때 끝내기
-                        if any(end_condition):
-                            break
+            for line in timeout_stdout:
+                if check_stop_events(stop_events):
+                    break
+                # while not stop_event.is_set():
+                end_condition = [f.tell() >= CollectorConfig.DUMP_FILESIZE_LIMIT,
+                                time.time() - start_time >= CollectorConfig.DUMP_TIME_LIMIT]
+                try:
+                    # Timeout 안걸렸을 때
+                    if line is not None:
+                        # 다음 splitter가 등장하면 현재 cell 쓰고 cell 초기화
+                        if log_cell_lines != "" and any([line.startswith(spliter) for spliter in CollectorConfig.LOG_CELL_SPLITER]):
+                            f.write(log_cell_lines)
+                            log_cell_lines = ""
+                        # line 내용물 모음
+                        log_cell_lines += line
+                    # Top의 경우 Splitter가 없기 때문에 Timeout 걸린 직후 다음 cell 기다리는 동안 씀
+                    elif log_cell_lines != "":
+                        if log_type == 'top':
+                            # Splitter 강제 주입
+                            log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
+                                log_cell_lines
+                            f.write(log_cell_lines)
+                            log_cell_lines = ""
 
-                    except Exception as e:
-                        logger.info(f'Error Msg: {traceback.format_exc()}')
-                        self.stop_event.set()
+                    # end 조건 만족할 때 끝내기
+                    if any(end_condition):
                         break
 
-                if chunk_count == CollectorConfig.SESSION_UPDATE_CHUNK_CNT or self.check_stop_events():
-                    is_finish = True
-                else:
-                    is_finish = False
-
-                # stop event로 종료 할 경우 남은 내용물 다 씀
-                # stop event로 종료한게 아니라면, 다음 루프에서 log_cell_lines 내용물들이 다 써지게 되어있음
-                if self.check_stop_events() and log_cell_lines != "":
-                    # top 의 경우 제일 윗줄에 Timestamp 넣어서 씀
-                    if self.log_type == 'top':
-                        log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
-                            log_cell_lines
-                    f.write(log_cell_lines)
-                    log_cell_lines = ""
-
-                file_size = f.tell()
-                file_name = f.name
-                if file_size > 0:
-                    logger.info(f'{f.name} {self.logging_session_id} finish dump file / size : {file_size}Byte')
-                    self.upload_queue.put((f.name, self.logging_session_id, chunk_count, is_finish, collector_chunk_start_time))
-                    chunk_count += 1
-                else:
-                    logger.info(f'{f.name} {self.logging_session_id} skip this file. {file_size}Byte')
-                    os.remove(file_name)
-                    time.sleep(2)
-                    # finish collection & close this connection
-
-                # 먼저 stdout이 비정상적으로 끊어지는 경우가 있다. (콜드부팅 등의 경우에) 이럴땐 그냥 나가기!
-                if stdout_stop_event.is_set():
+                except Exception as e:
+                    logger.info(f'Error Msg: {traceback.format_exc()}')
+                    stop_event.set()
                     break
 
-        self.status = 'stopping'
-        logger.info(f"{self.logging_session_id} finish collection")
+            if chunk_count == CollectorConfig.SESSION_UPDATE_CHUNK_CNT or check_stop_events(stop_events):
+                is_finish = True
+            else:
+                is_finish = False
 
-        # clear stdout and conn
-        stdout_stop_event.set()
-        close_client(conn)
-        del conn
-        del timeout_stdout
-        del stdout
+            # stop event로 종료 할 경우 남은 내용물 다 씀
+            # stop event로 종료한게 아니라면, 다음 루프에서 log_cell_lines 내용물들이 다 써지게 되어있음
+            if check_stop_events(stop_events) and log_cell_lines != "":
+                # top 의 경우 제일 윗줄에 Timestamp 넣어서 씀
+                if log_type == 'top':
+                    log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
+                        log_cell_lines
+                f.write(log_cell_lines)
+                log_cell_lines = ""
 
-        return
+            file_size = f.tell()
+            file_name = f.name
+            if file_size > 0:
+                logger.info(f'{f.name} {logging_session_id} finish dump file / size : {file_size}Byte')
+                upload_queue.put((f.name, logging_session_id, chunk_count, is_finish, collector_chunk_start_time))
+                chunk_count += 1
+            else:
+                logger.info(f'{f.name} {logging_session_id} skip this file. {file_size}Byte')
+                os.remove(file_name)
+                time.sleep(2)
+                # finish collection & close this connection
+
+            # 먼저 stdout이 비정상적으로 끊어지는 경우가 있다. (콜드부팅 등의 경우에) 이럴땐 그냥 나가기!
+            if stdout_stop_event.is_set():
+                break
+
+    status = 'stopping'
+    logger.info(f"{logging_session_id} finish collection")
+
+    # clear stdout and conn
+    stdout_stop_event.set()
+    close_client(conn)
+    del conn
+    del timeout_stdout
+    del stdout
+
+    return
