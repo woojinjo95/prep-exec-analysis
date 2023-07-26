87ae291d75614164ebe58c5d6663f7d928f9e247
정보미 <jeongbomi@nextlab.co.kr>
2023-07-26 13:44:59 +0900

    Button variant 옵션 추가

--- frontend/src/global/ui/Button/index.tsx
+++ frontend/src/global/ui/Button/index.tsx
@@ -5,17 +5,24 @@ import Text from '../Text'
 
 interface ButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   children: React.ReactNode
+  variant?: 'outline' | 'unstyled'
 }
 
 /**
  * 버튼 컴포넌트
  */
-const Button: React.FC<ButtonProps> = ({ children, type = 'button', ...props }) => {
+const Button: React.FC<ButtonProps> = ({ children, type = 'button', variant = 'outline', ...props }) => {
   return (
     <button
       // eslint-disable-next-line react/button-has-type
       type={type}
-      className={cx('shadow-[0px_1px_5px_#7777775E] px-4 py-1 rounded-full inline-flex items-center', props.className)}
+      className={cx(
+        'px-4 py-1 rounded-full inline-flex items-center hover:bg-slate-100 active:bg-slate-200 transition-colors',
+        {
+          'shadow-[0px_1px_5px_#7777775E]': variant === 'outline',
+        },
+        props.className,
+      )}
       {...props}
     >
       {typeof children === 'string' && <Text size="sm">{children}</Text>}
