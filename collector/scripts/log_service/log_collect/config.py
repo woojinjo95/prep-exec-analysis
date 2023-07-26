a104c90f3512e3882d41a2ea63cf3b11eaa415f4
kjune1236 <kjune1236@gmail.com>
2023-07-26 19:26:01 +0900

    fix config

--- collector/scripts/log_service/log_collect/config.py
+++ collector/scripts/log_service/log_collect/config.py
@@ -6,6 +6,6 @@ from typing import Tuple
 class CollectorConfig:
     DUMP_FILESIZE_LIMIT: int = (1024 * 1024 * 1)  # bytes
     # DUMP_TIME_LIMIT: float = 60 * 15  # seconds
-    DUMP_TIME_LIMIT: float = 20
+    DUMP_TIME_LIMIT: float = 60
     LOG_CELL_SPLITER: Tuple[str] = ("[ ", "Timestamp")
     LOG_STREAM_TIMEOUT: int = 1
