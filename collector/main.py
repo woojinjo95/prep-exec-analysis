a07681c326e2307e6178023cd162ef97b63c0207
kjune1236 <kjune1236@gmail.com>
2023-07-26 19:59:39 +0900

    fix code

--- collector/main.py
+++ collector/main.py
@@ -18,32 +18,39 @@ logger = logging.getLogger('main')
 
 ##### TEST #####
 def dumper():
-    connection_info = {
-        'host': '192.168.30.25',
-        'port': 5555,
-        'username': 'root',
-        'password': '',
-        'connection_mode': 'adb',
-    }
-    manager = LogFileManager(connection_info=connection_info)
-    manager.start()
-    time.sleep(86400)  # 1 day
-    manager.stop()
+    try:
+        connection_info = {
+            'host': '192.168.30.25',
+            'port': 5555,
+            'username': 'root',
+            'password': '',
+            'connection_mode': 'adb',
+        }
+        manager = LogFileManager(connection_info=connection_info)
+        manager.start()
+        time.sleep(86400)  # 1 day
+        manager.stop()
+    except Exception as e:
+        logger.info(e)
+    finally:
+        if manager:
+            manager.stop()
 
 
 def searcher():
     while True:
         # get 5 minutes ago logs
-        start = datetime.now() - timedelta(minutes=2, seconds=0)
-        end = datetime.now() - timedelta(minutes=1, seconds=0)
+        start = datetime.now() - timedelta(minutes=1, seconds=0)
+        end = datetime.now() - timedelta(minutes=0, seconds=0)
         # get 1000 lines
         logs = load_page(start.timestamp(), end.timestamp(), 1, 1000)
-        print(f'start : {start}, end : {end}, start_log : {logs[0]}, end_log : {logs[-1]}, len : {len(logs)}')
+        print(f'start : {start}, end : {end}, start_log : {logs[0] if len(logs) > 0 else ""}, end_log : {logs[-1] if len(logs) > 0 else ""}, len : {len(logs)}')
         time.sleep(10)
 
 
 searcher_thrd = Thread(target=searcher)
 searcher_thrd.start()
+
 dumper()
 
 
