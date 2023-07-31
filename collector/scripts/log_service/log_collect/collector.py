e28bc838f3fe207ec2e0e6eee79425179fc2b5bd
kjune1236 <kjune1236@gmail.com>
2023-07-31 16:46:25 +0900

    rollback collect code

--- collector/scripts/log_service/log_collect/collector.py
+++ collector/scripts/log_service/log_collect/collector.py
@@ -22,8 +22,9 @@ completed_log_dir = os.path.join('datas', 'stb_logs', 'completed_logs')
 
 def write_with_time_prefix(file: TextIO, line: str):
     cur_time = time.time()
-    new_line = f'<{cur_time}> {line}'
+    new_line = f'<Collector: {cur_time}> {line}'
     file.write(new_line)
+    # print(new_line)
 
 
 def collect(connection_info: dict, command_script: str, log_type: str, stop_event: Event):
@@ -51,24 +52,22 @@ def collect(connection_info: dict, command_script: str, log_type: str, stop_even
                     end_condition = [f.tell() >= CollectorConfig.DUMP_FILESIZE_LIMIT,
                                     time.time() - start_time >= CollectorConfig.DUMP_TIME_LIMIT]
                     try:
-                        print(line)
-                        # # Timeout 안걸렸을 때
-                        # if line is not None:
-                        #     # 다음 splitter가 등장하면 현재 cell 쓰고 cell 초기화
-                        #     if log_cell_lines != "" and any([line.startswith(spliter) for spliter in CollectorConfig.LOG_CELL_SPLITER]):
-                        #         write_with_time_prefix(f, log_cell_lines)
-                        #         print(log_cell_lines)
-                        #         log_cell_lines = ""
-                        #     # line 내용물 모음
-                        #     log_cell_lines += line
-                        # # Top의 경우 Splitter가 없기 때문에 Timeout 걸린 직후 다음 cell 기다리는 동안 씀
-                        # elif log_cell_lines != "":
-                        #     if log_type == 'top':
-                        #         # Splitter 강제 주입
-                        #         log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
-                        #             log_cell_lines
-                        #         write_with_time_prefix(f, log_cell_lines)
-                        #         log_cell_lines = ""
+                        # Timeout 안걸렸을 때
+                        if line is not None:
+                            # 다음 splitter가 등장하면 현재 cell 쓰고 cell 초기화
+                            if log_cell_lines != "" and any([line.startswith(spliter) for spliter in CollectorConfig.LOG_CELL_SPLITER]):
+                                write_with_time_prefix(f, log_cell_lines)
+                                log_cell_lines = ""
+                            # line 내용물 모음
+                            log_cell_lines += line
+                        # Top의 경우 Splitter가 없기 때문에 Timeout 걸린 직후 다음 cell 기다리는 동안 씀
+                        elif log_cell_lines != "":
+                            if log_type == 'top':
+                                # Splitter 강제 주입
+                                log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
+                                    log_cell_lines
+                                write_with_time_prefix(f, log_cell_lines)
+                                log_cell_lines = ""
 
                         # end 조건 만족할 때 끝내기
                         if any(end_condition):
@@ -78,13 +77,13 @@ def collect(connection_info: dict, command_script: str, log_type: str, stop_even
                         logger.info(f'Error Msg: {traceback.format_exc()}')
                         break
 
-                # if log_cell_lines != "":
-                #     # top 의 경우 제일 윗줄에 Timestamp 넣어서 씀
-                #     if log_type == 'top':
-                #         log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
-                #             log_cell_lines
-                #     write_with_time_prefix(f, log_cell_lines)
-                #     log_cell_lines = ""
+                if log_cell_lines != "":
+                    # top 의 경우 제일 윗줄에 Timestamp 넣어서 씀
+                    if log_type == 'top':
+                        log_cell_lines = f"Timestamp : {str(datetime.now() - timedelta(seconds=CollectorConfig.LOG_STREAM_TIMEOUT))}\n" + \
+                            log_cell_lines
+                    write_with_time_prefix(f, log_cell_lines)
+                    log_cell_lines = ""
 
                 file_size = f.tell()
                 file_name = f.name
