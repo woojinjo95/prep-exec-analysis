af3704e094659f4b143305a585da68586f36260d
kjune1236 <kjune1236@gmail.com>
2023-07-31 18:37:51 +0900

    add timeit

--- collector/scripts/util/common.py
+++ collector/scripts/util/common.py
@@ -193,3 +193,15 @@ def ordinal_suffix(n: int) -> str:
 
 def check_stop_events(stop_events) -> bool:
     return any([stop_event.is_set() for stop_event in stop_events if hasattr(stop_event, 'is_set')])
+
+
+def timeit(func: callable):
+    @wraps(func)
+    def timeit_wrapper(*args, **kwargs):
+        start_time = time.perf_counter()
+        result = func(*args, **kwargs)
+        end_time = time.perf_counter()
+        total_time = end_time - start_time
+        logger.info(f'Function {func.__name__} Took {total_time:.4f} seconds')
+        return result
+    return timeit_wrapper
