94c0b992da16fb3ff14d650e677ecc9d7ae75433
정보미 <jeongbomi@nextlab.co.kr>
2023-07-31 11:02:01 +0900

    글로벌 타입선언 수정

--- frontend/src/vite-env.d.ts
+++ frontend/src/vite-env.d.ts
@@ -1,11 +1,41 @@
+import React from 'react'
+
 /// <reference types="vite/client" />
 /// <reference types="vite-plugin-svgr/client" />
 
 declare global {
+  declare module '*.scss' {
+    const styles: {
+      [key: string]: string
+    }
+    export default styles
+  }
+  declare module '*.css' {
+    const styles: {
+      [key: string]: string
+    }
+    export default styles
+  }
+  declare module '*.png' {
+    const value: string
+    export = value
+  }
+  declare module '*.jpg' {
+    const value: string
+    export = value
+  }
+  declare module '*.svg' {
+    export const ReactComponent: React.FC<React.SVGProps<SVGSVGElement>>
+    const src: string
+    export default src
+  }
+  declare module '*.wav' {
+    const value: string
+    export = value
+  }
   interface ImportMetaEnv {
     readonly VITE_BACKEND_URL: string
   }
-
   interface ImportMeta {
     readonly env: ImportMetaEnv
   }
