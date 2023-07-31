2294b1fc72b9fd0b0eab6ce625ebd3a2b07ac489
김진환 <0316kjh@nextlab.co.kr>
2023-07-31 15:56:07 +0900

    #28 Add - init 리모컨 기본 정보 등록 기능 및 기본 정보 json

--- backend/app/app/initial_data.py
+++ backend/app/app/initial_data.py
@@ -1,7 +1,11 @@
 
 import logging
+import json
+import uuid
 
+# from PIL import Image
 from app.db.redis_session import RedisClient
+from app.crud.base import insert_to_mongodb
 
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
@@ -23,8 +27,53 @@ def init_hardware_configuration():
             RedisClient.hset(f'hardware_configuration', k, v)
 
 
+def init_remocon_registration():
+    class remocon_code_basic():
+        name: str
+        hotkey: list
+        code: str
+        location: [{
+            "x1": int,
+            "y1": int
+            },{
+            "x2": int,
+            "y2": int
+            }
+        ]
+    class remocon_basic():
+        id: str
+        name: str
+        image_path: str
+        image_resolution: {
+            "width": int,
+            "height": int
+        }
+        remocon_codes: list[remocon_code_basic]
+
+    with open('/app/app/remocon_ir_preset.json') as f:
+        preset_json = json.load(f)
+        for company, codes in preset_json.items():
+            remocon = remocon_basic()
+            remocon.id = str(uuid.uuid4())
+            remocon.name = company
+            # remocon.image_path = f'/app/app/files/image/remocon_image_{company}.jpg'
+            # image_size = Image.open(remocon.image_path).size
+            # remocon.image_resolution = {"width":image_size[0], "height":image_size[1]}
+            remocon_codes = []
+            for preset in codes["default"]:
+                remocon_code = remocon_code_basic()
+                remocon_code.name = preset.get('name', '')
+                remocon_code.code = preset.get('pronto_code', '')
+                # remocon_code.hotkey = preset.get('', [])
+                # remocon_code.location = [{"x1": 1, "y1": 1}, {"x2": 2, "y2": 2}]
+                remocon_codes.append(remocon_code)
+            remocon.remocon_codes = remocon_codes
+            insert_to_mongodb(col='remocon', data=remocon)
+
+
 def init() -> None:
     init_hardware_configuration()
+    init_remocon_registration()
 
 
 def main() -> None:
