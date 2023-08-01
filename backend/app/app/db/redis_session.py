6ae0569871a8c00a4511034cff42858874d6b178
minmax <minmax@nextlab.co.kr>
2023-08-01 20:28:34 +0900

    fix password

--- backend/app/app/db/redis_session.py
+++ backend/app/app/db/redis_session.py
@@ -2,7 +2,7 @@ from redis import Redis
 from app.core.config import settings
 
 
-if settings.REDIS_PASSWORD:
+if len(settings.REDIS_PASSWORD) > 0:
     RedisClient = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
                     charset="utf-8", db=settings.REDIS_DB, password=settings.REDIS_PASSWORD, decode_responses=True)
 else:
