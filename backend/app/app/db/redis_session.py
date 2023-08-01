ba9ac650c6e12df57a77e351461468a3f229f7f2
minmax <minmax@nextlab.co.kr>
2023-08-01 20:24:17 +0900

    add stblog ci

--- backend/app/app/db/redis_session.py
+++ backend/app/app/db/redis_session.py
@@ -2,5 +2,9 @@ from redis import Redis
 from app.core.config import settings
 
 
-RedisClient = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
+if settings.REDIS_PASSWORD:
+    RedisClient = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
+                    charset="utf-8", db=settings.REDIS_DB, password=settings.REDIS_PASSWORD, decode_responses=True)
+else:
+    RedisClient = Redis(host=settings.REDIS_HOST, port=settings.REDIS_PORT,
                     charset="utf-8", db=settings.REDIS_DB, decode_responses=True)
