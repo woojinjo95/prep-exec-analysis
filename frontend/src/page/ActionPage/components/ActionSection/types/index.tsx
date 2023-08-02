c173b82ce5a236f739fa38c8827cf89e93779034
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-02 15:40:32 +0900

    api 폴더 위치 변경 및 url, func, entity 구현

--- frontend/src/page/ActionPage/components/ActionSection/types/index.tsx
+++ frontend/src/page/ActionPage/components/ActionSection/types/index.tsx
@@ -1,10 +1,2 @@
-// refIdx가 포함된 block type
-export interface Block {
-  id: string
-  type: string
-  delay_time: number
-  value: string
-  refIdx: number
-}
 
 export type ActionStatus = 'RFC' | 'playing' | 'normal'
