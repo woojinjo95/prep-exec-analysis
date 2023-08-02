df305cdc74252c3229327317e6f22712eb49dce2
정보미 <jeongbomi@nextlab.co.kr>
2023-08-02 14:46:49 +0900

    하드웨어 설정 조회 api 추가 등

---
+++ frontend/src/page/ActionPage/components/TerminalSection/api/url.ts
@@ -0,0 +1,9 @@
+type ApiName = 'hardware_configuration'
+
+const apiUrls: {
+  [key in ApiName]: string
+} = {
+  hardware_configuration: '/api/v1/hardware_configuration',
+}
+
+export default apiUrls
--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -4,7 +4,7 @@ export default {
   theme: {
     extend: {
       colors: {
-        black: '#202020',
+        black: '#222222',
       },
     },
   },
