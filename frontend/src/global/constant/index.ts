e78bb679d567c4ca3bb7ee4dce9786e3cc3562d4
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-02 15:39:52 +0900

    ms 변환 관련 함수, 상수 추가

--- frontend/src/global/constant/index.ts
+++ frontend/src/global/constant/index.ts
@@ -4,3 +4,6 @@ import PagePath from './pagePath'
  * 웹사이트 접속 시 메인페이지
  */
 export const DEFAULT_PAGE_PATH = PagePath.action
+
+export const millisecondsPerSecond = 1000
+export const millisecondsPerMinute = 60 * millisecondsPerSecond
