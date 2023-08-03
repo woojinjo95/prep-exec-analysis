1b6976013a8d1dd172fb487ce42fe1fe265d5b51
정보미 <jeongbomi@nextlab.co.kr>
2023-08-03 18:51:20 +0900

    on/off 컨트롤 부분 변경 api call

--- frontend/src/page/ActionPage/components/TerminalSection/components/SettingsAndControlsPanel/OnOffControl.tsx
+++ frontend/src/page/ActionPage/components/TerminalSection/components/SettingsAndControlsPanel/OnOffControl.tsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import { ToggleButton } from '@global/ui'
+import { useToast } from '@chakra-ui/react'
+import { useMutation } from 'react-query'
 import { useHardwareConfiguration } from '../../api/hook'
+import { putHardwareConfiguration } from '../../api/func'
 
 const OnOffControl: React.FC = () => {
-  const { hardwareConfiguration } = useHardwareConfiguration()
+  const toast = useToast({ duration: 3000, isClosable: true })
+  const { hardwareConfiguration, refetch } = useHardwareConfiguration()
+  const { mutate: updateHardwareConfiguration } = useMutation(putHardwareConfiguration, {
+    onSuccess: () => {
+      refetch()
+    },
+    onError: () => {
+      toast({ status: 'error', title: 'An error has occurred. Please try again.' })
+    },
+  })
 
   return (
     <div className="bg-white h-fit">
@@ -12,15 +24,24 @@ const OnOffControl: React.FC = () => {
       <ul>
         <li>
           <span>DUT Power</span>
-          <ToggleButton isOn={!!hardwareConfiguration?.enable_dut_power} />
+          <ToggleButton
+            isOn={!!hardwareConfiguration?.enable_dut_power}
+            onClick={(isOn) => updateHardwareConfiguration({ enable_dut_power: isOn })}
+          />
         </li>
         <li>
           <span>HDMI(HPD)</span>
-          <ToggleButton isOn={!!hardwareConfiguration?.enable_hdmi} />
+          <ToggleButton
+            isOn={!!hardwareConfiguration?.enable_hdmi}
+            onClick={(isOn) => updateHardwareConfiguration({ enable_hdmi: isOn })}
+          />
         </li>
         <li>
           <span>DUT WAN</span>
-          <ToggleButton isOn={!!hardwareConfiguration?.enable_dut_wan} />
+          <ToggleButton
+            isOn={!!hardwareConfiguration?.enable_dut_wan}
+            onClick={(isOn) => updateHardwareConfiguration({ enable_dut_wan: isOn })}
+          />
         </li>
       </ul>
     </div>
