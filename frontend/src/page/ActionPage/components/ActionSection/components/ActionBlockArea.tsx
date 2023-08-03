468835738bd61263fbee206fbe14185a6153fdb1
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-03 15:30:48 +0900

    BlockControl ui 적용 및 overflow 처리

--- frontend/src/page/ActionPage/components/ActionSection/components/ActionBlockArea.tsx
+++ frontend/src/page/ActionPage/components/ActionSection/components/ActionBlockArea.tsx
@@ -260,7 +260,7 @@ const ActionBlockArea = (): JSX.Element => {
           onMouseMove={handleMouseMove}
           style={{ backgroundImage: `url(${BackgroundImage})` }}
         >
-          {blocks && blockDummys && (
+          {blocks && blockDummys && blocks.length > 0 && (
             <div className="w-full h-full pl-[30px] pr-[30px] overflow-y-auto pt-[2px] pb-[2px]">
               <DragDropContext onDragEnd={handleDragEnd}>
                 <Droppable droppableId="droppable">
@@ -315,6 +315,13 @@ const ActionBlockArea = (): JSX.Element => {
                   )}
                 </Droppable>
               </DragDropContext>
+            </div>
+          )}
+
+          {blocks && blockDummys && blocks.length === 0 && (
+            <div className="h-full w-full justify-center items-center">
+              <p className="text-xl">No Blocks</p>
+              <p className="text-base">Start action about device control and adb/ssh access</p>
             </div>
           )}
 
