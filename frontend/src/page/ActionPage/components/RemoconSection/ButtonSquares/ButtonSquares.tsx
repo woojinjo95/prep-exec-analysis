db0c70ab22dd7152de44c6768303cf8df4510fb9
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-03 19:23:10 +0900

    반응형 대비 좌표값 반영하도록 수정

--- frontend/src/page/ActionPage/components/RemoconSection/ButtonSquares/ButtonSquares.tsx
+++ frontend/src/page/ActionPage/components/RemoconSection/ButtonSquares/ButtonSquares.tsx
@@ -1,44 +1,60 @@
 import { KeyEvent } from '@page/ActionPage/types'
-import React, { useEffect, useState } from 'react'
+import React, { forwardRef, useEffect, useState } from 'react'
+import cx from 'classnames'
 
 interface ButtonSquaresProps {
   keyEvent: KeyEvent | null
   keyboardCoors?: { leftTop: { left: number; top: number }; rightBottom: { right: number; bottom: number } }[]
+  imageRef: React.RefObject<HTMLImageElement>
+  remoconResolution: [number, number]
 }
 
-const ButtonSquares = ({ keyEvent, keyboardCoors }: ButtonSquaresProps): JSX.Element => {
-  const [isSquareVisible, setIsSquareVisible] = useState<boolean>(false)
+const ButtonSquares = forwardRef(
+  ({ keyEvent, keyboardCoors, imageRef, remoconResolution }: ButtonSquaresProps): JSX.Element => {
+    const [isSquareVisible, setIsSquareVisible] = useState<boolean>(false)
 
-  useEffect(() => {
-    if (keyEvent?.altKey && keyEvent.code === 'KeyR') {
-      setIsSquareVisible(true)
-    }
-    if (!keyEvent?.altKey) {
-      setIsSquareVisible(false)
-    }
-  }, [keyEvent])
+    useEffect(() => {
+      if (keyEvent?.altKey) {
+        setIsSquareVisible(true)
+      }
+      if (!keyEvent?.altKey) {
+        setIsSquareVisible(false)
+      }
+    }, [keyEvent])
 
-  return (
-    <div className="relative">
-      {keyboardCoors &&
-        isSquareVisible &&
-        keyboardCoors.map((coor) => {
-          return (
-            <div
-              key={`button_square_${coor.leftTop.left}_${coor.leftTop.top}`}
-              className="absolute border-orange-300 border-2"
-              style={{
-                display: isSquareVisible ? '' : 'none',
-                top: coor.leftTop.top,
-                left: coor.leftTop.left,
-                height: coor.rightBottom.bottom - coor.leftTop.top,
-                width: coor.rightBottom.right - coor.leftTop.left,
-              }}
-            />
-          )
-        })}
-    </div>
-  )
-}
+    return (
+      <div>
+        {imageRef.current &&
+          keyboardCoors &&
+          keyboardCoors.map((coor) => {
+            return (
+              <div
+                key={`button_square_${coor.leftTop.left}_${coor.leftTop.top}`}
+                className={cx('absolute cursor-pointer', {
+                  'border-orange-300 border-2': isSquareVisible,
+                })}
+                style={{
+                  top:
+                    imageRef.current!.getBoundingClientRect().top +
+                    coor.leftTop.top * (imageRef.current!.getBoundingClientRect().height / remoconResolution[1]),
+                  left:
+                    imageRef.current!.getBoundingClientRect().left +
+                    coor.leftTop.left * (imageRef.current!.getBoundingClientRect().width / remoconResolution[0]),
+                  height:
+                    coor.rightBottom.bottom *
+                      (imageRef.current!.getBoundingClientRect().height / remoconResolution[1]) -
+                    coor.leftTop.top * (imageRef.current!.getBoundingClientRect().height / remoconResolution[1]),
+                  width:
+                    coor.rightBottom.right * (imageRef.current!.getBoundingClientRect().width / remoconResolution[0]) -
+                    coor.leftTop.left * (imageRef.current!.getBoundingClientRect().width / remoconResolution[0]),
+                }}
+              />
+            )
+          })}
+      </div>
+    )
+  },
+)
 
+ButtonSquares.displayName = 'ButtonSquares'
 export default ButtonSquares
