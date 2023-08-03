24b0491ebd997dc785a0d52374550260f0be3887
woojinjo <e05062@nextlab.co.kr>
2023-08-03 14:07:21 +0900

    add mongo create - and envs - not crud, just create only - update requirement

---
+++ media/scripts/connection/mongo_db/create.py
@@ -0,0 +1,35 @@
+import pymongo
+from pymongo.collection import Collection
+from typing import Tuple
+
+from .config import Settings
+
+for el in vars(Settings):
+    if el.startswith('__'):
+        pass
+    else:
+        print(el, getattr(Settings, el))
+
+
+def conn_mongodb() -> pymongo.MongoClient:
+    client = pymongo.MongoClient(
+        f"mongodb://{Settings.MONGODB_USERNAME}:{Settings.MONGODB_PASSWORD}@{Settings.MONGODB_SERVER}:{Settings.MONGODB_PORT}/{Settings.MONGODB_NAME}?authSource={Settings.MONGODB_AUTHENTICATION_SOURCE}&readPreference=primary&ssl=false")
+    return client
+
+
+def get_mongodb_collection(col: str) -> Collection:
+    db = Settings.MONGODB_NAME
+    client = conn_mongodb()
+    result_db = client[db]
+    target_collection = result_db[col]
+    return target_collection
+
+
+def insert_to_mongodb(col: str, data: dict) -> Tuple[bool, str]:
+    mongo_client = get_mongodb_collection(col)
+    res = mongo_client.insert_one(data)
+    # type(res) == pymongo.results.InsertOneResult
+
+    inserted_id = res.inserted_id
+    acknowledged = res.acknowledged
+    return acknowledged, inserted_id
