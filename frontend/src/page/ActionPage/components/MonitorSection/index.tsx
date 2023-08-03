5948b808df39ed9023697f2735d877a953745d65
정보미 <jeongbomi@nextlab.co.kr>
2023-08-03 18:05:57 +0900

    스트리밍에 의한 버벅거림 픽스

--- frontend/src/page/ActionPage/components/MonitorSection/index.tsx
+++ frontend/src/page/ActionPage/components/MonitorSection/index.tsx
@@ -8,6 +8,9 @@ const MonitorSection: React.FC = () => {
   return (
     <section className="w-full h-full border border-black relative">
       <HLSPlayer
+        autoPlay
+        controls
+        className="h-full aspect-video bg-black"
         src={import.meta.env.VITE_STREAMING_URL || `${window.location.protocol}//${window.location.hostname}:8888/live`}
       />
     </section>
