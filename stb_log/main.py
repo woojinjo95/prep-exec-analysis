2137619830e2ff893e250a9ab7e341cb12bb7a86
kjune1236 <kjune1236@gmail.com>
2023-08-03 19:52:47 +0900

    fix main.py

--- stb_log/main.py
+++ stb_log/main.py
@@ -1,8 +1,5 @@
-import time
 import logging
 from multiprocessing import Queue
-import threading
-import traceback
 
 from scripts.log_service.log_helper import LogHelper, init_log_helper, terminate_log_helper
 from scripts.log_service.log_manage.log_manager import LogFileManager
@@ -14,7 +11,6 @@ from scripts.config.constant import RedisChannel, RedisDB
 log_queue = Queue(maxsize=10000)
 log_helper = LogHelper()
 init_log_helper(log_helper, log_queue)
-
 logger = logging.getLogger('main')
 
 
@@ -29,40 +25,42 @@ connection_info = {
 log_type = 'logcat'
 
 
-manager = LogFileManager(connection_info=connection_info, log_type=log_type)
+manager = None
 
 
-def command_parser(command: dict, stop_event: threading.Event):
+def command_parser(command: dict):
     ''' 
     PUBLISH command '{"streaming": "start"}'
     PUBLISH command '{"streaming": "stop"}'
     '''
+    global manager
 
     if command.get('streaming'):
         streaming_arg = command.get('streaming')
         logger.info(f'command_parser: {streaming_arg}')
 
         if streaming_arg == 'start':
-            if manager.is_alive():
-                logger.warning('LogFileManager is already alive')
-                return
+            if manager and manager.is_alive():
+                logger.warning('Manager is already alive')
             else:
-                logger.info('start_manager')
+                manager = LogFileManager(connection_info=connection_info, log_type=log_type)
                 manager.start()
+                logger.info('Start Manager')
 
         elif streaming_arg == 'stop':
-            logger.info('stop_manager')
-            manager.stop()
+            if manager and manager.is_alive():
+                manager.stop()
+                logger.info('Stop Manager')
+            else:
+                logger.warning('Manager is not alive')
 
         else:
             logger.warning(f'Unknown streaming args: {streaming_arg}')
 
 
-stop_event = threading.Event()
-
 with get_strict_redis_connection(RedisDB.hardware) as src:
     for command in Subscribe(src, RedisChannel.command):
-        command_parser(command, stop_event)
+        command_parser(command)
 
 
 terminate_log_helper(log_helper, log_queue)
