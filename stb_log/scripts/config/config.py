8ff714afe89107b2611fe961ed621d23592b02b1
kjune1236 <kjune1236@gmail.com>
2023-08-03 18:00:11 +0900

    fix get and set value

--- stb_log/scripts/config/config.py
+++ stb_log/scripts/config/config.py
@@ -1,13 +1,13 @@
 from scripts.connection.redis import (get_strict_redis_connection,
                                            hget_value, hset_value)
- 
+from .constant import RedisDB
 
 
-def get_value(db: int, key: str, field: str = None, default: any = None) -> any:
+def get_value(key: str, field: str = None, default: any = None, db: int = RedisDB.stb_log) -> any:
     with get_strict_redis_connection(db) as src:
         return hget_value(src, key, field, default)
 
 
-def set_value(db: int, key: str, field: str = None, value: any = None):
+def set_value(key: str, field: str = None, value: any = None, db: int = RedisDB.stb_log):
     with get_strict_redis_connection(db) as src:
         hset_value(src, key, field, value)
--- stb_log/scripts/config/constant.py
+++ stb_log/scripts/config/constant.py
@@ -4,3 +4,6 @@ from dataclasses import dataclass
 @dataclass
 class RedisDB:
     hardware: int = 0
+    stb_log: int = 4
+    
+    
