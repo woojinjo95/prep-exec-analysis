0cc84371a31b1ec017277491bcf8782f8ab346fa
kjune1236 <kjune1236@gmail.com>
2023-08-04 14:42:35 +0900

    remove comment

--- stb_log/scripts/log_service/log_collect/config.py
+++ stb_log/scripts/log_service/log_collect/config.py
@@ -5,8 +5,6 @@ from scripts.config.config import get_setting_with_env
 @dataclass
 class CollectorConfig:
     DUMP_FILESIZE_LIMIT: int = (1024 * 1024 * 1)  # bytes
-    # DUMP_TIME_LIMIT: float = 60 * 15  # seconds
-    # DUMP_TIME_LIMIT: float = 10
     DUMP_TIME_LIMIT: float = get_setting_with_env("DUMP_TIME_LIMIT")
     LOG_CELL_SPLITER: Tuple[str] = ("[ ", "Timestamp")
     LOG_STREAM_TIMEOUT: int = 1
