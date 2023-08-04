5e51dfd7adf21d14750ad2d80ff8d6115c3f756b
kjune1236 <kjune1236@gmail.com>
2023-08-04 14:29:04 +0900

    move redis conn function and fix to insert_to_many

--- stb_log/scripts/config/config.py
+++ stb_log/scripts/config/config.py
@@ -1,20 +1,6 @@
 import os
 from ast import literal_eval
 
-from scripts.connection.redis_conn import (get_strict_redis_connection,
-                                           hget_value, hset_value)
-from .constant import RedisDB
-
-
-def get_value(key: str, field: str = None, default: any = None, db: int = RedisDB.stb_log) -> any:
-    with get_strict_redis_connection(db) as src:
-        return hget_value(src, key, field, default)
-
-
-def set_value(key: str, field: str = None, value: any = None, db: int = RedisDB.stb_log):
-    with get_strict_redis_connection(db) as src:
-        hset_value(src, key, field, value)
-
 
 def get_setting_with_env(key: str, default: str = None):
     try:
--- stb_log/scripts/connection/redis_conn.py
+++ stb_log/scripts/connection/redis_conn.py
@@ -4,6 +4,7 @@ from ast import literal_eval
 
 from redis import StrictRedis
 from scripts.config.config import get_setting_with_env
+from scripts.config.constant import RedisDB
 
 
 logger = logging.getLogger('connection')
@@ -51,3 +52,13 @@ def hset_value(sr_connection: StrictRedis, key: str, field: str, value: any):
     if value == 'None' or value == None:
         logger.warning(f'Nonetype or string "None" is interpreted as key is not exist.')
     value = sr_connection.hset(key, field, str(value))
+
+
+def get_value(key: str, field: str = None, default: any = None, db: int = RedisDB.stb_log) -> any:
+    with get_strict_redis_connection(db) as src:
+        return hget_value(src, key, field, default)
+
+
+def set_value(key: str, field: str = None, value: any = None, db: int = RedisDB.stb_log):
+    with get_strict_redis_connection(db) as src:
+        hset_value(src, key, field, value)
