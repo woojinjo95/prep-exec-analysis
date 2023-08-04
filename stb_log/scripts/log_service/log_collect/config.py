e89c91a3833e2c8d971e3df8646bddb5e80d20e3
kjune1236 <kjune1236@gmail.com>
2023-08-04 13:30:43 +0900

    fix thread to process maintainer

--- stb_log/scripts/log_service/log_collect/config.py
+++ stb_log/scripts/log_service/log_collect/config.py
@@ -1,12 +1,12 @@
 from dataclasses import dataclass
 from typing import Tuple
-
+import os
 
 @dataclass
 class CollectorConfig:
     DUMP_FILESIZE_LIMIT: int = (1024 * 1024 * 1)  # bytes
     # DUMP_TIME_LIMIT: float = 60 * 15  # seconds
     # DUMP_TIME_LIMIT: float = 10
-    DUMP_TIME_LIMIT: float = 60
+    DUMP_TIME_LIMIT: float = os.getenv("DUMP_TIME_LIMIT")
     LOG_CELL_SPLITER: Tuple[str] = ("[ ", "Timestamp")
     LOG_STREAM_TIMEOUT: int = 1
