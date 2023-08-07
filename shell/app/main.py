fdb399418e005f4358e9e2b8b7c79e578b03c7b1
minmax <minmax@nextlab.co.kr>
2023-08-07 18:02:33 +0900

    add init

---
+++ shell/app/main.py
@@ -0,0 +1,101 @@
+from fastapi import FastAPI
+from fastapi.responses import HTMLResponse
+from fastapi.websockets import WebSocket, WebSocketDisconnect
+import asyncio
+import redis.asyncio as redis
+from adbutils import adb
+import logging
+
+
+logging.config.fileConfig('./app/logging.conf', disable_existing_loggers=False)
+logger = logging.getLogger(__name__)
+
+app = FastAPI()
+
+
+CHANNEL_NAME = 'shell'
+html = """
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Chat</title>
+    </head>
+    <body>
+        <h1>WebSocket Chat</h1>
+        <form action="" onsubmit="sendMessage(event)">
+            <input type="text" id="messageText" autocomplete="off"/>
+            <button>Send</button>
+        </form>
+        <ul id='messages'>
+        </ul>
+        <script>
+            var host = window.location.hostname;
+            var ws = new WebSocket(`ws://${host}:5000/api/v1/client/ws`);
+            ws.onmessage = function(event) {
+                var messages = document.getElementById('messages')
+                var message = document.createElement('li')
+                var content = document.createTextNode(event.data)
+                message.appendChild(content)
+                messages.appendChild(message)
+            };
+            function sendMessage(event) {
+                var input = document.getElementById("messageText")
+                ws.send(input.value)
+                input.value = ''
+                event.preventDefault()
+            }
+        </script>
+    </body>
+</html>
+"""
+
+
+@app.get("/", tags=['websocket'])
+def testclient():
+    return HTMLResponse(html)
+
+
+@app.websocket('/ws')
+async def ws_voting_endpoint(websocket: WebSocket):
+    await websocket.accept()
+    await adb_connector(websocket)
+
+
+async def adb_connector(websocket: WebSocket):
+    async def consumer_handler(conn: any, ws: WebSocket):
+        try:
+            while True:
+                message = await ws.receive_text()
+                if message is not None:  # command:로 시작하는 메시지를 control 채널로 발행함
+                    await conn.publish(CHANNEL_NAME, message)
+        except WebSocketDisconnect as exc:
+            logger.error(exc)
+        except Exception as exc:
+            logger.error(exc)
+
+    async def producer_handler(pubsub: any, ws: WebSocket):
+        await pubsub.subscribe(CHANNEL_NAME)  # control 채널을 수신함
+        try:
+            while True:
+                message = await pubsub.get_message(ignore_subscribe_messages=True)
+                if message:
+                    await ws.send_text(message.get('data'))
+        except Exception as exc:
+            logger.error(exc)
+
+    conn = await get_adb_connection()
+    pubsub = conn.pubsub()
+
+    consumer_task = consumer_handler(conn=conn, ws=websocket)
+    producer_task = producer_handler(pubsub=pubsub, ws=websocket)
+    done, pending = await asyncio.wait(
+        [consumer_task, producer_task], return_when=asyncio.FIRST_COMPLETED,
+    )
+    logger.debug(f"Done task: {done}")
+    for task in pending:
+        logger.debug(f"Canceling task: {task}")
+        task.cancel()
+
+
+def get_adb_connection():
+    return adb.connect("192.168.1.208:5555")
