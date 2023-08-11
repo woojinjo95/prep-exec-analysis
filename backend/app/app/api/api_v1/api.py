d6de8ab87a86723b25e3a1aaba3cae42a1e4306e
조은비 <eunbi0322@nextlab.co.kr>
2023-08-11 17:08:09 +0900

    api 이름 수정

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -6,13 +6,17 @@ api_router = APIRouter()
 api_router.include_router(hardware_configuration.router, prefix="/hardware_configuration",
                           tags=["hardware_configuration"])
 api_router.include_router(remocon.router, prefix="/remocon", tags=["remocon"])
+
 api_router.include_router(scenario.router, prefix="/scenario", tags=["scenario"])
 api_router.include_router(block.router, prefix="/scenario/block", tags=["scenario"])
 api_router.include_router(block.router_detail, prefix="/scenario/block_group", tags=["scenario"])
-api_router.include_router(file.router, prefix="/file", tags=["file"])
+
 api_router.include_router(analysis_config.router, prefix="/analysis_config", tags=["analysis_config"])
-api_router.include_router(client.router, prefix="/client", tags=["client"])
+
 api_router.include_router(trace.router, prefix="/trace", tags=["trace"])
 api_router.include_router(shell.router, prefix="/shell", tags=["shell"])
-api_router.include_router(utility.router, prefix="/utility", tags=["utility"])
 api_router.include_router(analysis_result.router, prefix="/analysis_result", tags=["analysis_result"])
+
+api_router.include_router(utility.router, tags=["utility"])
+api_router.include_router(file.router, prefix="/file", tags=["utility"])
+api_router.include_router(client.router, prefix="/client", tags=["utility"])
