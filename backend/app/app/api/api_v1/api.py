4128e807699c189fd926c617e5a8caee51cf48f0
조은비 <eunbi0322@nextlab.co.kr>
2023-08-11 16:56:59 +0900

    #이름변경

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -1,9 +1,10 @@
-from app.api.api_v1.endpoints import item, remocon, scenario, hardware_configuration, block, file, client, analysis_config, trace, terminal, utility, analysis_result
+from app.api.api_v1.endpoints import item, remocon, scenario, hardware_configuration, block, file, client, analysis_config, trace, shell, utility, analysis_result
 from fastapi import APIRouter
 
 api_router = APIRouter()
 # api_router.include_router(item.router, prefix="/item", tags=["item"])
-api_router.include_router(hardware_configuration.router, prefix="/hardware_configuration", tags=["hardware_configuration"])
+api_router.include_router(hardware_configuration.router, prefix="/hardware_configuration",
+                          tags=["hardware_configuration"])
 api_router.include_router(remocon.router, prefix="/remocon", tags=["remocon"])
 api_router.include_router(scenario.router, prefix="/scenario", tags=["scenario"])
 api_router.include_router(block.router, prefix="/scenario/block", tags=["scenario"])
@@ -12,6 +13,6 @@ api_router.include_router(file.router, prefix="/file", tags=["file"])
 api_router.include_router(analysis_config.router, prefix="/analysis_config", tags=["analysis_config"])
 api_router.include_router(client.router, prefix="/client", tags=["client"])
 api_router.include_router(trace.router, prefix="/trace", tags=["trace"])
-api_router.include_router(terminal.router, prefix="/terminal", tags=["terminal"])
+api_router.include_router(shell.router, prefix="/shell", tags=["shell"])
 api_router.include_router(utility.router, prefix="/utility", tags=["utility"])
 api_router.include_router(analysis_result.router, prefix="/analysis_result", tags=["analysis_result"])
--- backend/app/app/api/api_v1/endpoints/shell.py
+++ backend/app/app/api/api_v1/endpoints/shell.py
@@ -9,8 +9,8 @@ logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.get("", response_model=schemas.TerminalList)
-def get_terminal_modes() -> schemas.TerminalList:
+@router.get("", response_model=schemas.ShellList)
+def get_shell_modes() -> schemas.ShellList:
     """
     터미널 목록
     """
@@ -20,18 +20,18 @@ def get_terminal_modes() -> schemas.TerminalList:
 
 
 
-@router.get("/logs", response_model=schemas.TerminalLogList)
-def get_terminal_logs(
-    terminal_mode: ShellTypeEnum,
+@router.get("/logs", response_model=schemas.ShellLogList)
+def get_shell_logs(
+    shell_mode: ShellTypeEnum,
     shell_id: str,
     start_time: str = Query(..., description="ex.2009-02-13T23:31:30"),
     end_time: str = Query(..., description="ex.2009-02-13T23:31:30"),
-    ) -> schemas.TerminalLogList:
+    ) -> schemas.ShellLogList:
     """
     터미널별 일정기간 로그 조회
     """
 
-    pipeline = [{'$match': {'time': {'$gte': start_time, '$lte': end_time}, 'mode': terminal_mode.value, 'shell_id': shell_id}},
+    pipeline = [{'$match': {'time': {'$gte': start_time, '$lte': end_time}, 'mode': shell_mode.value, 'shell_id': shell_id}},
                 {'$project': {'_id': 0, 'lines': 1}},
                 {'$unwind': {'path': '$lines'}},
                 {'$group': {'_id': None, 'lines': {'$push': '$lines'}}}]
