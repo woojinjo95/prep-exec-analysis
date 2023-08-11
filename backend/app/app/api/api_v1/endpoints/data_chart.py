f60b07f2c43484840dde0d6a1fc457697ca703fa
김진환 <0316kjh@nextlab.co.kr>
2023-08-11 14:48:08 +0900

    #94 Add - 데이터 차트 조회 API 개별화

--- backend/app/app/api/api_v1/endpoints/data_chart.py
+++ backend/app/app/api/api_v1/endpoints/data_chart.py
@@ -1,26 +1,24 @@
 import logging
 
-from datetime import datetime, timedelta
 from app import schemas
 from app.crud.base import aggregate_from_mongodb
-from app.api.utility import get_multi_or_paginate_by_res, load_from_mongodb
-from fastapi import APIRouter, HTTPException, Query
+from app.api.utility import load_from_mongodb
+from fastapi import APIRouter, Query
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
 
 
-@router.get("")
-def get_data_of_chart(
+# Log Level Finder
+@router.get("/log_level_finder", response_model=schemas.LogLevelFinder)
+def get_data_of_log_level_finder(
     start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
     end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
     ):
     """
-    데이터 차트 내용 조회
+    로그 레벨 데이터 조회
     """
-    pipeline = [{'$match': {'time': {'$gte': start_time, '$lte': end_time}}}]
-    # Log Level Finder
-    log_level_finder_pipeline = pipeline + [
+    log_level_finder_pipeline = [
         {'$match': {'time': {'$gte': start_time, '$lte': end_time}}}, 
         {'$project': {'_id': 0, 'lines': 1}},
         {'$unwind': {'path': '$lines'}}, 
@@ -28,31 +26,110 @@ def get_data_of_chart(
         {'$project': {'timestamp': 1, 'log_level': 1}}
     ]
     log_level_finder = aggregate_from_mongodb(col='stb_log', pipeline=log_level_finder_pipeline)
-
-    # CPU, Memory
-    cpu_memory = load_from_mongodb(col="cpu_memory", param={'time': {'$gte': start_time, '$lte': end_time}})
-
-    # Color Reference
-    # Event Log
-    # Video Analysis Result
-    # Log Pattern Maching
-    # Measurement
-    # Process Lifecycle
-    # Network Filter
-
-    return {"items": {"log_level_finder": log_level_finder, "cpu_memory": cpu_memory}}
+    return {"items": log_level_finder}
 
 
-
-@router.get("")
+# CPU, Memory
+@router.get("/cpu_and_memory", response_model=schemas.CpuAndMemory)
 def get_data_of_cpu_and_memory(
     start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
     end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
     ):
     """
-    Cpu, Memory 데이터 차트 내용 조회
+    Cpu, Memory 데이터 조회
     """
-    pipeline = [{'$match': {'time': {'$gte': start_time, '$lte': end_time}}}]
-    
-    result = aggregate_from_mongodb(col='####', pipeline=pipeline)
-    return result
+    time_range_param = {'time': {'$gte': start_time, '$lte': end_time}}
+    cpu_and_memory = load_from_mongodb(col="stb_info", param=time_range_param, proj={'_id': 0})
+    return {"items": cpu_and_memory}
+
+
+# Color Reference
+@router.get("/color_reference", response_model=schemas.ColorReference)
+def get_data_of_color_reference(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    컬러 레퍼런스 데이터 조회
+    """
+    color_reference = load_from_mongodb()
+    return {"items": color_reference}
+
+
+# Event Log
+@router.get("/event_log", response_model=schemas.EventLog)
+def get_data_of_event_log(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    이벤트 로그 데이터 조회
+    """
+    event_log = load_from_mongodb()
+    return {"items": event_log}
+
+
+# Video Analysis Result
+@router.get("/video_analysis_result", response_model=schemas.VideoAnalysisResult)
+def get_data_of_video_analysis_result(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    비디오 분석 결과 데이터 조회
+    """
+    video_analysis_result = load_from_mongodb()
+    return {"items": video_analysis_result}
+
+
+# Log Pattern Maching
+@router.get("/log_pattern_matching", response_model=schemas.LogPatternMatching)
+def get_data_of_log_pattern_matching(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    로그 패턴 매칭 데이터 조회
+    """
+    log_pattern_matching = load_from_mongodb()
+    return {"items": log_pattern_matching}
+
+
+# Measurement
+@router.get("/measurement", response_model=schemas.Measurement)
+def get_data_of_measurement(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    분석 데이터 조회
+    """
+    measurement = load_from_mongodb()
+    return {"items": measurement}
+
+
+# Process Lifecycle
+@router.get("/process_lifecycle", response_model=schemas.ProcessLifecycle)
+def get_data_of_process_lifecycle(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    프로세스 활동주기 데이터 조회
+    """
+    process_lifecycle = load_from_mongodb()
+    return {"items": process_lifecycle}
+
+
+# Network Filter
+@router.get("/network_filter", response_model=schemas.NetworkFilter)
+def get_data_of_network_filter(
+    start_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    end_time: str = Query(..., description='ex)2009-02-13T23:31:30+00:00'),
+    ):
+    """
+    네트워크 필터 데이터 조회
+    """
+    network_filter = load_from_mongodb()
+    return {"items": network_filter}
+
