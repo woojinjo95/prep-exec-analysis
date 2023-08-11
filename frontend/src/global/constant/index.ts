3b74eb0cfe33db49fa7ef077f5a7e57a6901a27e
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-11 11:47:13 +0900

    page size 관련 constants 추가

--- frontend/src/global/constant/index.ts
+++ frontend/src/global/constant/index.ts
@@ -12,3 +12,8 @@ export const MILLISECONDS_PER_MINUTE = 60 * MILLISECONDS_PER_SECOND
  * 유효한 IP 문자열인지 체크하는 정규표현식
  */
 export const IPRegex = /(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)(\.(25[0-5]|2[0-4]\d|1\d\d|[1-9]?\d)){3}/
+
+export const PAGE_SIZE_TWENTY = 20
+export const PAGE_SIZE_FIFTEEN = 15
+export const PAGE_SIZE_TWELVE = 12
+export const PAGE_SIZE_TEN = 10
