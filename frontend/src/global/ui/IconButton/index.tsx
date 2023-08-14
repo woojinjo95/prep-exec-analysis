2b2ee1653f9d759c2984cfabfb6a72460ef43957
정보미 <jeongbomi@nextlab.co.kr>
2023-08-14 13:50:30 +0900

    IconButton 공통 UI 컴포넌트 추가

---
+++ frontend/src/global/ui/IconButton/index.tsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import classnames from 'classnames/bind'
+import styles from './IconButton.module.scss'
+
+const cx = classnames.bind(styles)
+
+interface IconButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
+  icon?: React.ReactNode
+  colorScheme?: 'charcoal' | 'light'
+  className?: string
+}
+
+const IconButton: React.FC<IconButtonProps> = ({
+  icon,
+  colorScheme = 'light',
+  type = 'button',
+  className,
+  ...props
+}) => {
+  return (
+    <button
+      // eslint-disable-next-line react/button-has-type
+      type={type}
+      className={cx(
+        'w-[52px] h-10 rounded-full flex justify-center items-center border',
+        {
+          'bg-charcoal': colorScheme === 'charcoal',
+          'border-light-charcoal': colorScheme === 'charcoal',
+          'bg-white': colorScheme === 'light',
+          'border-light-grey': colorScheme === 'light',
+        },
+        className,
+      )}
+      {...props}
+    >
+      <div className={cx('icon-button-icon', colorScheme)}>{icon}</div>
+    </button>
+  )
+}
+
+export default IconButton
