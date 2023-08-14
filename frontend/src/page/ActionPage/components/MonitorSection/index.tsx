9478bcd3278f45953f46ef071b088c25cd7553a9
정보미 <jeongbomi@nextlab.co.kr>
2023-08-14 13:51:13 +0900

    액션 페이지 레이아웃 조정, 배경색상 적용 등

--- frontend/src/page/ActionPage/components/MonitorSection/index.tsx
+++ frontend/src/page/ActionPage/components/MonitorSection/index.tsx
@@ -6,11 +6,11 @@ import HLSPlayer from './components/HLSPlayer'
  */
 const MonitorSection: React.FC = () => {
   return (
-    <section className="w-full h-full border-b border-light-charcoal relative">
+    <section className="w-full h-full border-b border-light-charcoal relative aspect-video grid justify-center items-center bg-black">
       <HLSPlayer
         autoPlay
         controls
-        className="h-full aspect-video bg-black"
+        className="h-full aspect-video"
         src={import.meta.env.VITE_STREAMING_URL || `${window.location.protocol}//${window.location.hostname}:8888/live`}
       />
     </section>
