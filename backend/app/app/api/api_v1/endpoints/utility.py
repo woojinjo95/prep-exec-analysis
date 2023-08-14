c7a5ad679903979a76da0b4d3945c20f03625d2a
김진환 <0316kjh@nextlab.co.kr>
2023-08-14 10:54:42 +0900

    #130 Add - 워크스페이스 비디오 API 이전

--- backend/app/app/api/api_v1/endpoints/utility.py
+++ backend/app/app/api/api_v1/endpoints/utility.py
@@ -1,8 +1,10 @@
 import logging
 
 from app import schemas
+from app.crud.base import aggregate_from_mongodb
 from app.db.redis_session import RedisClient
-from fastapi import APIRouter, HTTPException, Query
+from fastapi import APIRouter, HTTPException
+from fastapi.responses import StreamingResponse
 
 logger = logging.getLogger(__name__)
 router = APIRouter()
@@ -14,3 +16,26 @@ def read_timezone() -> schemas.Timezone:
     타임존 확인
     """
     return {'timezone': RedisClient.hget('common', 'timezone')}
+
+
+@router.get("/video")
+def read_video_file() -> StreamingResponse:
+    """
+    비디오 파일 재생
+    """
+    scenario_id = RedisClient.hget('testrun', 'scenario_id')
+    if scenario_id is None:
+        raise HTTPException(status_code=404, detail="Scenario data not found")
+    pipeline = [{'$match': {'id': scenario_id}},
+                {'$project': {'_id': 0, 'videos': '$testrun.raw.videos'}},
+                ]
+    video_info = aggregate_from_mongodb(col='scenario', pipeline=pipeline)
+    if not video_info:
+        raise HTTPException(status_code=404, detail="Scenario data not found")
+    video_info = video_info[0]['videos'][0]
+    video_file_path = video_info.get('path', '')
+    video_file_path = video_file_path.replace('./data', '/app')
+    def video_file_open():
+        with open(f"{video_file_path}", mode="rb") as video_file:
+            yield from video_file
+    return StreamingResponse(video_file_open(), media_type="video/mp4")
