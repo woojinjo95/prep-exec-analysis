0918fb144c221a3e758435c529ea40217b073414
kjune1236 <kjune1236@gmail.com>
2023-08-16 18:50:29 +0900

    add analyzer default packages

---
+++ analyzer/main.py
@@ -0,0 +1,113 @@
+import logging
+
+from scripts.log_service.logcat.log_manager import LogcatManager
+from scripts.log_service.dumpsys.manager import DumpsysManager
+from scripts.connection.redis_conn import get_strict_redis_connection
+from scripts.connection.redis_pubsub import Subscribe
+from scripts.config.constant import RedisChannel, RedisDB
+from scripts.log_service.log_organizer import LogOrganizer
+
+
+logger = logging.getLogger('main')
+
+
+logcat_manager = None
+dumpsys_manager = None
+connection_info = {}
+
+
+def start_logcat_manager(connection_info: dict):
+    global logcat_manager
+
+    if logcat_manager is not None:
+        logger.warning('LogcatManager is already alive')
+    else:
+        logcat_manager = LogcatManager(connection_info=connection_info)
+        logcat_manager.start()
+        logger.info('Start LogcatManager')
+
+
+def stop_logcat_manager():
+    global logcat_manager
+
+    if logcat_manager is not None:
+        logcat_manager.stop()
+        logcat_manager = None
+        logger.info('Stop LogcatManager')
+    else:
+        logger.warning('LogcatManager is not alive')
+
+
+def start_dumpsys_manager(connection_info: dict):
+    global dumpsys_manager
+
+    if dumpsys_manager is not None:
+        logger.warning('DumpsysManager is already alive')
+    else:
+        dumpsys_manager = DumpsysManager(connection_info=connection_info)
+        dumpsys_manager.start()
+        logger.info('Start DumpsysManager')
+
+
+def stop_dumpsys_manager():
+    global dumpsys_manager
+
+    if dumpsys_manager is not None:
+        dumpsys_manager.stop()
+        dumpsys_manager = None
+        logger.info('Stop DumpsysManager')
+    else:
+        logger.warning('DumpsysManager is not alive')
+
+
+def command_parser(command: dict):
+    ''' 
+    start: PUBLISH command '{"msg": "stb_log", "data": {"control": "start"}}'
+    stop: PUBLISH command '{"msg": "stb_log", "data": {"control": "stop"}}'
+    connection info: PUBLISH command '{"msg": "config", "data": {"mode": "adb", "host": "192.168.30.30", "port": "5555", "username": "root", "password": ""}}'
+    '''
+    global connection_info
+
+    if command.get('msg') == 'stb_log':
+        arg = command.get('data', {})
+        logger.info(f'msg: stb_log. arg: {arg}')
+
+        control = arg.get('control', '')
+        if control == 'start':
+            start_logcat_manager(connection_info)
+            start_dumpsys_manager(connection_info)
+        elif control == 'stop':
+            stop_logcat_manager()
+            stop_dumpsys_manager()
+        else:
+            logger.warning(f'Unknown control: {control}')
+
+    if command.get('msg') == 'config':
+        data = command.get('data')
+        data['connection_mode'] = data['mode']
+        del data['mode']
+        connection_info = data
+        logger.info(f'connection_info: {connection_info}')
+
+
+def main():
+    with get_strict_redis_connection(RedisDB.hardware) as src:
+        for command in Subscribe(src, RedisChannel.command):
+            command_parser(command)
+
+
+if __name__ == '__main__':
+    try:
+        log_organizer = LogOrganizer(name='stb_log')
+        log_organizer.set_stream_logger('main')
+        log_organizer.set_stream_logger('connection')
+        log_organizer.set_stream_logger('collector')
+        log_organizer.set_stream_logger('logcat')
+        log_organizer.set_stream_logger('dumpsys')
+        logger.info('Start stb_log container')
+
+        main()
+
+    finally:
+        logger.info('Close stb_log container')
+        log_organizer.close()
---
+++ analyzer/scripts/util/process_maintainer.py
@@ -0,0 +1,70 @@
+import logging
+import threading
+import gc
+from multiprocessing import Event, Process
+
+
+logger = logging.getLogger('main')
+
+
+class ProcessMaintainer:
+    def __init__(self, target, kwargs=(), revive_interval=None, daemon=True):
+        self.init_property()
+        self.target = target
+        self.kwargs = kwargs
+        self.revive_interval = revive_interval
+        self.daemon = daemon
+
+    def init_property(self):
+        self.process = None
+        self.reviver = None
+
+    def start(self):
+        if self.process:
+            raise Exception('duplicate processes')
+
+        self.process = Process(target=self.target, kwargs=self.kwargs)
+        self.process.daemon = self.daemon
+        self.process.start()
+        # If interval is set, run the revive task
+        if self.revive_interval:
+            self.revive(self.revive_interval)
+
+    def terminate(self):
+        if self.process:
+            # Stop revive task first
+            if self.reviver:
+                self.reviver.cancel()
+
+            # Stop process on running state
+            if self.process.is_alive():
+                self.process.terminate()
+
+            # Wait whether forced or normal exit
+            self.process.join(timeout=5)
+            self.init_property()
+
+    # Periodic task when interval is given, also it can be done immediately without interval as needed
+    def revive(self, interval=None):
+        if not self.process.is_alive():
+            logger.info(f'Revive {self.target.__name__}() process.')
+            self.terminate()
+            self.start()
+            interval = None
+
+        # If interval is set, run timer to execute task after a few seconds
+        if interval:
+            self.reviver = threading.Timer(self.revive_interval, self.revive, args=(interval, ))
+            self.reviver.daemon = True
+            self.reviver.start()
+
+        gc.collect()
+
+    def is_alive(self):
+        if self.process and self.process.is_alive():
+            return True
+        else:
+            return False
+
+    def __del__(self):
+        self.terminate()
