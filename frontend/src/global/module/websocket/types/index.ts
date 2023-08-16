b6ae82a319d7baa0d236476cb1d3e4164ba057e4
정보미 <jeongbomi@nextlab.co.kr>
2023-08-16 19:09:46 +0900

    websocket time 관련 수정

--- frontend/src/global/module/websocket/types/index.ts
+++ frontend/src/global/module/websocket/types/index.ts
@@ -7,10 +7,15 @@ interface StopScenarioMessage {
   msg: 'stop_scenario'
 }
 
+interface OnOffControlMessage {
+  msg: 'on_off_control'
+  data: { [key in 'enable_dut_power' | 'enable_hdmi' | 'enable_dut_wan']?: boolean }
+}
+
 export type PublishMessage = {
-  level: 'debug' | 'info' | 'warning' | 'error' | 'critical' | 'fatal'
-  time: number
-} & (RunScenarioMessage | StopScenarioMessage)
+  level?: 'debug' | 'info' | 'warning' | 'error' | 'critical' | 'fatal'
+  time?: number
+} & (RunScenarioMessage | StopScenarioMessage | OnOffControlMessage)
 
 export type SubscribeMessage = {
   level: 'debug' | 'info' | 'warning' | 'error' | 'critical' | 'fatal'
