2f1e0e0b2d9aeda747706045fe01254c78de73b1
kjune1236 <kjune1236@gmail.com>
2023-08-17 13:52:06 +0900

    add freeze detect module

---
+++ analyzer/scripts/modules/freeze_detect.py
@@ -0,0 +1,32 @@
+import logging
+from multiprocessing import Process
+
+
+logger = logging.getLogger('freeze_detect')
+
+
+class FreezeDetect:
+    def __init__(self):
+        self.processor = None
+
+    def __start_processor(self):
+        self.processor = Process(target=detect_freeze, kwargs={
+        }, daemon=True, revive_interval=10)
+        self.processor.start()
+
+    def __stop_processor(self):
+        self.processor.terminate()
+
+    def start(self):
+        if self.processor and self.processor.is_alive():
+            logger.warning(f'{self.__class__.__name__} is already alive')
+        else:
+            self.__start_processor()
+            logger.info(f'{self.__class__.__name__} start')
+
+    def stop(self):
+        if self.processor and self.processor.is_alive():
+            self.__stop_processor()
+            logger.info(f'{self.__class__.__name__} stop')
+        else:
+            logger.warning(f'{self.__class__.__name__} is not alive')
