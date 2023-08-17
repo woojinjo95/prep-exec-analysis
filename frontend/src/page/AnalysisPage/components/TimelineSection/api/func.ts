486830f764703cab160eeae242e78d9318caa7dc
정보미 <jeongbomi@nextlab.co.kr>
2023-08-17 12:53:28 +0900

    오타 수정

--- frontend/src/page/AnalysisPage/components/TimelineSection/api/func.ts
+++ frontend/src/page/AnalysisPage/components/TimelineSection/api/func.ts
@@ -5,13 +5,13 @@ import { CPUAndMemory } from './entity'
 import apiUrls from './url'
 
 /**
- * CPU, Memory 사용량 리스트 조회 api
+ * CPU, Memory 사용률 리스트 조회 api
  */
 export const getCPUAndMemory = async (params: { scenario_id?: string; start_time: string; end_time: string }) => {
   try {
     const result = await API.get<Response<CPUAndMemory[]>>(apiUrls.cpu_and_memory, { params })
 
-    return result.data
+    return result.data.items
   } catch (err) {
     const er = err as AxiosError
     throw er
--- frontend/src/page/AnalysisPage/components/TimelineSection/api/hook.ts
+++ frontend/src/page/AnalysisPage/components/TimelineSection/api/hook.ts
@@ -2,7 +2,7 @@ import { useQuery } from 'react-query'
 import { getCPUAndMemory } from './func'
 
 /**
- * CPU, Memory 사용량 리스트 조회 hook
+ * CPU, Memory 사용률 리스트 조회 hook
  */
 export const useCPUAndMemory = (params: Parameters<typeof getCPUAndMemory>[0]) => {
   const { data, isLoading, refetch } = useQuery(['cpu_and_memory', params], () => getCPUAndMemory(params))
