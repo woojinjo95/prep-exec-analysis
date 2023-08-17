0c24fec397c4e242d4d498060eab6016caae8086
kjune1236 <kjune1236@gmail.com>
2023-08-17 17:04:48 +0900

    inherit module

--- analyzer/scripts/modules/color_reference.py
+++ analyzer/scripts/modules/color_reference.py
@@ -1,34 +1,12 @@
 import logging
 
-from multiprocessing import Process
+from .template import Module
 from scripts.processor.color_reference import process
 
 
 logger = logging.getLogger('color_reference')
 
 
-class ColorReference:
+class ColorReference(Module):
     def __init__(self):
-        self.processor = None
-
-    def __start_processor(self):
-        self.processor = Process(target=process, kwargs={
-        }, daemon=True)
-        self.processor.start()
-
-    def __stop_processor(self):
-        self.processor.terminate()
-
-    def start(self):
-        if self.processor and self.processor.is_alive():
-            logger.warning('ColorReference is already alive')
-        else:
-            self.__start_processor()
-            logger.info('ColorReference start')
-
-    def stop(self):
-        if self.processor and self.processor.is_alive():
-            self.__stop_processor()
-            logger.info('ColorReference stop')
-        else:
-            logger.warning('ColorReference is not alive')
+        super().__init__(func=process)
--- analyzer/scripts/modules/freeze_detect.py
+++ analyzer/scripts/modules/freeze_detect.py
@@ -1,33 +1,12 @@
 import logging
-from multiprocessing import Process
+
+from .template import Module
 from scripts.processor.freeze_detect import detect_freeze
 
 
 logger = logging.getLogger('freeze_detect')
 
 
-class FreezeDetect:
+class FreezeDetect(Module):
     def __init__(self):
-        self.processor = None
-
-    def __start_processor(self):
-        self.processor = Process(target=detect_freeze, kwargs={
-        }, daemon=True)
-        self.processor.start()
-
-    def __stop_processor(self):
-        self.processor.terminate()
-
-    def start(self):
-        if self.processor and self.processor.is_alive():
-            logger.warning(f'{self.__class__.__name__} is already alive')
-        else:
-            self.__start_processor()
-            logger.info(f'{self.__class__.__name__} start')
-
-    def stop(self):
-        if self.processor and self.processor.is_alive():
-            self.__stop_processor()
-            logger.info(f'{self.__class__.__name__} stop')
-        else:
-            logger.warning(f'{self.__class__.__name__} is not alive')
+        super().__init__(func=detect_freeze)
