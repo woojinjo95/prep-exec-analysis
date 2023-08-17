45adad8a543fb96aaf813196d0a1cd6495ef94ab
kjune1236 <kjune1236@gmail.com>
2023-08-17 17:15:27 +0900

    fix command parser for freeze

--- analyzer/command.py
+++ analyzer/command.py
@@ -28,35 +28,27 @@ class CommandExecutor:
         self.freeze_detect_module.stop()
 
     def execute(self, command: Dict):
-        # color_reference start:  PUBLISH command '{"msg": "color_reference", "data": {"control": "start"}}'
-        # color_reference stop:  PUBLISH command '{"msg": "color_reference", "data": {"control": "stop"}}'
+        # freeze_detect start:  PUBLISH command '{"msg": "analysis", "data": {"measurement": ['freeze']}}'
 
-        # freeze_detect start:  PUBLISH command '{"msg": "freeze_detect", "data": {"control": "start"}}'
-        # freeze_detect stop:  PUBLISH command '{"msg": "freeze_detect", "data": {"control": "stop"}}'
+        # if command.get('msg') == 'color_reference':
+        #     arg = command.get('data', {})
+        #     logger.info(f'msg: color_reference. arg: {arg}')
 
-        if command.get('msg') == 'color_reference':
-            arg = command.get('data', {})
-            logger.info(f'msg: color_reference. arg: {arg}')
-
-            control = arg.get('control', '')
-            if control == 'start':
-                self.start_color_ref_module()
-            elif control == 'stop':
-                self.stop_color_ref_module()
-            else:
-                logger.warning(f'Unknown control: {control}')
+        #     control = arg.get('control', '')
+        #     if control == 'start':
+        #         self.start_color_ref_module()
+        #     elif control == 'stop':
+        #         self.stop_color_ref_module()
+        #     else:
+        #         logger.warning(f'Unknown control: {control}')
         
-        elif command.get('msg') == 'freeze_detect':
-            arg = command.get('data', {})
-            logger.info(f'msg: freeze_detect. arg: {arg}')
+        if command.get('msg') == 'analysis':
+            data = command.get('data', {})
+            logger.info(f'msg: analysis. data: {data}')
 
-            control = arg.get('control', '')
-            if control == 'start':
+            measurement = data.get('measurement', [])
+            if 'freeze' in measurement:
                 self.start_freeze_detect_module()
-            elif control == 'stop':
-                self.stop_freeze_detect_module()
-            else:
-                logger.warning(f'Unknown control: {control}')
 
         else:
             pass
