53d3b498847f0a6df0d77572dd6e7677c051bcfd
kjune1236 <kjune1236@gmail.com>
2023-08-17 19:34:42 +0900

    fix dummy path

--- analyzer/scripts/connection/external.py
+++ analyzer/scripts/connection/external.py
@@ -29,11 +29,11 @@ def load_input() -> Dict:
     # load data format to db
     data = {
         "path": "/app/workspace/testruns/2023-08-17T082451F848091/raw/videos/video_2023-08-17T170801F542707+0900_1800.mp4",
-        # "stat_path": "/app/workspace/testruns/2023-08-17T082451F848091/raw/videos/video_2023-08-17T170801F542707+0900_1800.mp4_stat",
+        "stat_path": "/app/workspace/testruns/2023-08-17T082451F848091/raw/videos/video_2023-08-17T170801F542707+0900_1800.mp4_stat",
     }
     return {
         'video_path': data['path'],
-        # 'json_data': json.load(open(data['stat_path'], 'r'))
+        'json_data': json.load(open(data['stat_path'], 'r'))
     }
 
 
