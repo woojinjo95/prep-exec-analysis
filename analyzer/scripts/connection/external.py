6926d3594f9987a0a48780a4a1e326d16a2ee167
kjune1236 <kjune1236@gmail.com>
2023-08-17 17:50:33 +0900

    fix external for dummy

--- analyzer/scripts/connection/external.py
+++ analyzer/scripts/connection/external.py
@@ -1,5 +1,6 @@
 from typing import Dict
 import time
+import json
 
 from scripts.connection.redis_conn import get_value
 from scripts.util._timezone import get_utc_datetime
@@ -25,10 +26,10 @@ def construct_report_data() -> Dict:
 def load_input() -> Dict:
     # load data format to db
     data = {
-        "path": "/app/workspace/test.mp4",
-        # "stat_path": "./data/workspace/testruns/2023-08-14T042445F738532/raw/videos/video_2023-08-14T181329F384025+0900_180.mp4_stat",
+        "path": "/app/workspace/testruns/2023-08-17T082451F848091/raw/videos/video_2023-08-17T170801F542707+0900_1800.mp4",
+        "stat_path": "/app/workspace/testruns/2023-08-17T082451F848091/raw/videos/video_2023-08-17T170801F542707+0900_1800.mp4_stat",
     }
     return {
         'video_path': data['path'],
-        # 'json_data': json.load(open(data['stat_path'], 'r'))
+        'json_data': json.load(open(data['stat_path'], 'r'))
     }
