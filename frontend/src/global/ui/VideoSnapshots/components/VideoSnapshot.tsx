6f896dbe7b3f9949ad3cd22844112b6d00f23e73
정보미 <jeongbomi@nextlab.co.kr>
2023-08-21 13:23:10 +0900

    video url - blob으로 변환, scrollbar에 따라 표시할 스냅샷 변경

--- frontend/src/global/ui/VideoSnapshots/components/VideoSnapshot.tsx
+++ frontend/src/global/ui/VideoSnapshots/components/VideoSnapshot.tsx
@@ -1,57 +1,54 @@
-/* eslint-disable no-param-reassign */
 import React, { useEffect, useState } from 'react'
 import { VIDEO_SNAPSHOT_HEIGHT } from '@global/ui/VideoSnapshots/constant'
+import { useCreateVideo } from '@global/ui/VideoSnapshots/hook'
 
 const useCaptureVideoFrame = ({
-  videoElement,
+  src,
   currentTime = 0,
   format = 'jpeg',
   quality = 0.92,
 }: {
-  videoElement: HTMLVideoElement | null
+  src: string | null
   currentTime: number
   format?: 'png' | 'jpeg'
   quality?: number
 }): {
   dataURI: string
 } => {
+  const { isLoadedVideo, videoRef } = useCreateVideo({ src, currentTime })
   const [dataURI, setDataURI] = useState<string>('')
 
   useEffect(() => {
-    if (!videoElement) return
-
-    videoElement.currentTime = currentTime
-    videoElement.muted = false
-    videoElement.crossOrigin = 'Anonymous'
+    if (!isLoadedVideo || !videoRef.current) return
 
     // canvas에 특정 시점의 video를 이미지로 draw
     const canvas = document.createElement('canvas')
-    canvas.width = (VIDEO_SNAPSHOT_HEIGHT * videoElement.videoWidth) / videoElement.videoHeight
+    canvas.width = (VIDEO_SNAPSHOT_HEIGHT * videoRef.current.videoWidth) / videoRef.current.videoHeight
     canvas.height = VIDEO_SNAPSHOT_HEIGHT
-    canvas.getContext('2d')?.drawImage(videoElement, 0, 0, canvas.width, canvas.height)
+    canvas.getContext('2d')?.drawImage(videoRef.current, 0, 0, canvas.width, canvas.height)
 
     const newDataURI = canvas.toDataURL(`image/${format}`, quality)
     setDataURI(newDataURI)
-  }, [videoElement])
+  }, [isLoadedVideo])
 
   return { dataURI }
 }
 
 interface VideoSnapshotProps {
+  src: string | null
   currentTime: number
   translateX: number
-  videoRef: React.MutableRefObject<HTMLVideoElement | null>
 }
 
 /**
  * 비디오 스냅샷 컴포넌트
  *
- * @param videoRef 원본 비디오 엘리먼트
+ * @param src 비디오 주소
  * @param currentTime 스냅샷으로 따고싶은 비디오 시간
  * @param translateX 스냅샷 표시 위치
  */
-const VideoSnapshot: React.FC<VideoSnapshotProps> = ({ videoRef, currentTime, translateX }) => {
-  const { dataURI } = useCaptureVideoFrame({ videoElement: videoRef.current, currentTime })
+const VideoSnapshot: React.FC<VideoSnapshotProps> = ({ src, currentTime, translateX }) => {
+  const { dataURI } = useCaptureVideoFrame({ src, currentTime })
 
   if (!dataURI) return <div />
   return (
--- frontend/src/global/ui/VideoSnapshots/index.tsx
+++ frontend/src/global/ui/VideoSnapshots/index.tsx
@@ -1,15 +1,14 @@
 import React, { useMemo, useRef } from 'react'
 import * as d3 from 'd3'
 
-import { useCreateVideo } from '@global/ui/VideoSnapshots/hook'
 import { VIDEO_SNAPSHOT_HEIGHT } from '@global/ui/VideoSnapshots/constant'
 import VideoSnapshot from './components/VideoSnapshot'
 
 interface VideoSnapshotsProps {
-  src: string | null
-  // TODO: startTime
-  // TODO: endTime
+  startMillisecond: number | null
+  endMillisecond: number | null
   tickCount?: number
+  src: string | null
 }
 
 /**
@@ -17,36 +16,31 @@ interface VideoSnapshotsProps {
  *
  * @param src 비디오 주소
  */
-const VideoSnapshots: React.FC<VideoSnapshotsProps> = ({ src, tickCount = 10 }) => {
+const VideoSnapshots: React.FC<VideoSnapshotsProps> = ({ startMillisecond, endMillisecond, tickCount = 10, src }) => {
   const divRef = useRef<HTMLDivElement | null>(null)
-  const { isLoadedVideo, videoRef } = useCreateVideo({ src, currentTime: 0 })
-  // 비디오 스냅샷 넓이
-  const videoSnapshotWidth = useMemo(() => {
-    if (!isLoadedVideo || !videoRef.current) return null
-    return (VIDEO_SNAPSHOT_HEIGHT * videoRef.current.videoWidth) / videoRef.current.videoHeight
-  }, [isLoadedVideo])
+
+  // video x축 scale
   const scaleX: d3.ScaleLinear<number, number, never> | null = useMemo(() => {
-    if (!isLoadedVideo || !videoRef.current || !divRef.current || !videoSnapshotWidth) return null
+    if (startMillisecond === null || endMillisecond === null || !divRef.current) return null
     return d3
       .scaleLinear()
-      .domain([0, Math.floor(videoRef.current?.duration)])
-      .range([0, divRef.current.clientWidth - videoSnapshotWidth])
-  }, [isLoadedVideo])
+      .domain([startMillisecond / 1000, endMillisecond / 1000])
+      .range([0, divRef.current.clientWidth])
+  }, [startMillisecond, endMillisecond])
 
-  if (!src) return <div />
+  if (!src) return <div ref={divRef} style={{ height: VIDEO_SNAPSHOT_HEIGHT }} />
   return (
     <div ref={divRef} className="relative" style={{ height: VIDEO_SNAPSHOT_HEIGHT }}>
-      {isLoadedVideo &&
-        scaleX?.ticks(tickCount).map((currentTime) => {
-          return (
-            <VideoSnapshot
-              key={`snapshot-${currentTime}`}
-              currentTime={currentTime}
-              translateX={scaleX(currentTime)}
-              videoRef={videoRef}
-            />
-          )
-        })}
+      {scaleX?.ticks(tickCount).map((currentTime) => {
+        return (
+          <VideoSnapshot
+            key={`snapshot-${currentTime}`}
+            currentTime={currentTime}
+            translateX={scaleX(currentTime)}
+            src={src}
+          />
+        )
+      })}
     </div>
   )
 }
