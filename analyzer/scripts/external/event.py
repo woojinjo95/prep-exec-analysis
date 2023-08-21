f0097cf4b6b8447708e84d4c79dec5232a796abc
kjune1236 <kjune1236@gmail.com>
2023-08-21 19:05:57 +0900

    fix boot_test log

--- analyzer/scripts/external/event.py
+++ analyzer/scripts/external/event.py
@@ -11,7 +11,7 @@ logger = logging.getLogger('connection')
 def get_data_of_event_log(start_time: datetime, end_time: datetime):
     scenario_info = get_scenario_info()
     scenario_id = scenario_info['scenario_id']
-    logger.info(f'scenario_id: {scenario_id}, start_time: {start_time}, end_time: {end_time}')
+    logger.info(f'get_data_of_event_log. scenario_id: {scenario_id}, start_time: {start_time}, end_time: {end_time}')
     
     event_log_pipeline = [
         {'$match': {'scenario_id': scenario_id,
