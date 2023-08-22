15e17a6a4b1e9ef4505f4df7c23b573651a4ac77
kjune1236 <kjune1236@gmail.com>
2023-08-22 16:26:22 +0900

    fix to crop_video_with_opencv

--- analyzer/scripts/processor/warm_boot.py
+++ analyzer/scripts/processor/warm_boot.py
@@ -8,7 +8,7 @@ from scripts.external.data import load_input
 from scripts.external.report import report_output
 from scripts.connection.redis_pubsub import publish_msg
 from scripts.util._timezone import get_utc_datetime
-from scripts.util.video import crop_video_with_timestamps
+from scripts.util.video import crop_video_with_opencv
 from scripts.external.event import get_data_of_event_log, get_remocon_times
 from scripts.config.config import get_setting_with_env
 from scripts.analysis.boot_test.diff import task_boot_test_with_diff
@@ -28,7 +28,7 @@ def test_warm_boot():
         logger.info(f'remocon_times: {remocon_times}')
 
         output_dir = os.path.join('/tmp', 'video', 'warm_boot')
-        crop_videos = crop_video_with_timestamps(args.video_path, args.timestamps, remocon_times, output_dir, get_setting_with_env('WARM_BOOT_DURATION', 10))
+        crop_videos = crop_video_with_opencv(args.video_path, args.timestamps, remocon_times, output_dir, get_setting_with_env('WARM_BOOT_DURATION', 10))
         for crop_video in crop_videos:
             result = task_boot_test_with_diff(crop_video.video_path, crop_video.timestamps, crop_video.timestamps[0])
             logger.info(f'result: {result}')
