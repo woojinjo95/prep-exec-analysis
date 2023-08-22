05b810a5e36fb3d6f5fdaa2274f701e491610e9d
조은비 <eunbi0322@nextlab.co.kr>
2023-08-22 09:05:56 +0900

    #

--- backend/app/app/api/api_v1/api.py
+++ backend/app/app/api/api_v1/api.py
@@ -11,7 +11,7 @@ api_router.include_router(scenario.router, prefix="/scenario", tags=["scenario"]
 api_router.include_router(block.router, prefix="/scenario/block", tags=["scenario"])
 api_router.include_router(block.router_detail, prefix="/scenario/block_group", tags=["scenario"])
 
-api_router.include_router(analysis_config.router, prefix="/analysis_config", tags=["analysis_config"])
+# api_router.include_router(analysis_config.router, prefix="/analysis_config", tags=["analysis_config"])
 
 api_router.include_router(trace.router, prefix="/trace", tags=["trace"])
 api_router.include_router(shell.router, prefix="/shell", tags=["shell"])
