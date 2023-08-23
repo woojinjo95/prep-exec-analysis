d96ca0c6c5122e3062c6fdc06f90f6a4dab6e819
kjune1236 <kjune1236@gmail.com>
2023-08-23 17:45:54 +0900

    fix roi test

--- analyzer/test/extract_frame.py
+++ analyzer/test/extract_frame.py
@@ -4,7 +4,7 @@ import os
 image_dir = './images'
 os.makedirs(image_dir, exist_ok=True)
 
-video_path = '/home/jade-m32-05/projects/prep-exec-analysis/data/workspace/testruns/2023-08-14T054428F718593/raw/videos/video_2023-08-22T113901F361442+0900_1800.mp4'
+video_path = '/home/jade-m32-05/projects/prep-exec-analysis/data/workspace/testruns/2023-08-14T054428F718593/raw/videos/video_2023-08-22T172921F075886+0900_1800.mp4'
 cap = cv2.VideoCapture(video_path)
 index = 0
 while True:
