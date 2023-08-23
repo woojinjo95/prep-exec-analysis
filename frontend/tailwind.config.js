540b97d2e5fa23716bd1ec1defb7b0cf599e85c2
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-23 13:25:59 +0900

    tailwind config 및 text color 관련 속성 추가

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -11,11 +11,13 @@ export default {
         'light-charcoal': '#4E525A',
         grey: '#8F949E',
         'light-grey': '#DFE0EE',
+        'dark-grey': '#707070',
 
         // palette
         pink: '#D89B9B',
         red: '#E93535',
         orange: '#FF9900',
+        'light-orange': '#FFBB00',
         yellow: '#FFDD00',
         navy: '#3A547D',
         green: '#97A442',
