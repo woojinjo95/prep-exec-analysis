e42fae5f12299ff0dbc0cfd24a5e9e543014739f
조은비 <eunbi0322@nextlab.co.kr>
2023-08-23 17:47:19 +0900

    서비스 상태 조회 API 추가

--- backend/app/app/api/api_v1/endpoints/utility.py
+++ backend/app/app/api/api_v1/endpoints/utility.py
@@ -14,3 +14,11 @@ def read_timezone() -> schemas.Timezone:
     타임존 확인
     """
     return {'timezone': RedisClient.hget('common', 'timezone')}
+
+
+@router.get("/service_state", response_model=schemas.ServiceState)
+def read_service_state() -> schemas.ServiceState:
+    """
+    Retrieve service_state.
+    """
+    return {'items': {'state': RedisClient.hget('common', 'service_state')}}
