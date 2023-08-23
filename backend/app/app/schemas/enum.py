68f084178263914590ccf8a734262591d06f82df
조은비 <eunbi0322@nextlab.co.kr>
2023-08-23 17:47:29 +0900

    enum 수정

--- backend/app/app/schemas/enum.py
+++ backend/app/app/schemas/enum.py
@@ -31,13 +31,11 @@ class ResumeTypeEnum(ExtendedEnum):
 
 class BootTypeEnum(ExtendedEnum):
     image_matching = "image_matching"
-    screen_change_rate = "screen_change_rate"
 
 
 class AnalysisTypeEnum(ExtendedEnum):
     freeze = "freeze"
     # macroblock = "macroblock"
-    loudness = "loudness"
     resume = "resume"
     boot = "boot"
     channel_change_time = "channel_change_time"
@@ -76,3 +74,10 @@ class LogModuleEnum(ExtendedEnum):
 class ShellModeEnum(ExtendedEnum):
     adb = "adb"
     ssh = "ssh"
+
+
+class ServiceStateEnum(ExtendedEnum):
+    idle = "idle"
+    streaming = "streaming"
+    playblock = "playblock"
+    analysis = "analysis"
