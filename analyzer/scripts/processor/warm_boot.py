c4cd3eb1f9817b9aae808f6b9c058831b04e157e
kjune1236 <kjune1236@gmail.com>
2023-08-23 14:03:45 +0900

    fix test_warm_boot for config

--- analyzer/scripts/processor/warm_boot.py
+++ analyzer/scripts/processor/warm_boot.py
@@ -3,7 +3,7 @@ import traceback
 import tempfile
 
 from scripts.format import CollectionName
-from scripts.external.data import load_input
+from scripts.external.data import load_input, read_analysis_config
 from scripts.external.report import report_output
 from scripts.connection.redis_pubsub import publish_msg
 from scripts.util._timezone import get_utc_datetime
@@ -19,29 +19,13 @@ logger = logging.getLogger('boot_test')
 
 @log_decorator(logger)
 def test_warm_boot():
-    try:  
-        args = load_input()
-
-        event_log = get_data_of_event_log(args.timestamps[0], args.timestamps[-1])
-        remocon_times = get_power_key_times(event_log)
-        logger.info(f'remocon_times: {remocon_times}')
-
-        with tempfile.TemporaryDirectory(dir='/tmp') as output_dir:
-            warm_boot_results = []
-            crop_videos = crop_video_with_opencv(args.video_path, args.timestamps, remocon_times, output_dir, get_setting_with_env('WARM_BOOT_DURATION', 10))
-            for crop_video in crop_videos:
-                if not check_poweroff_video(crop_video.video_path):
-                    continue
-                result = task_boot_test_with_diff(crop_video.video_path, crop_video.timestamps, crop_video.timestamps[0])
-                logger.info(f'result: {result}')
-                warm_boot_results.append(result)
-
-        for result in warm_boot_results:
-            if result['status'] == 'success':
-                report_output(CollectionName.WARM_BOOT.value, {
-                    'timestamp': get_utc_datetime(result['diff_timestamp']),
-                    'measure_time': result['diff_time'],
-                })
+    try:
+        analysis_config = read_analysis_config()
+        processing_mode = analysis_config['resume']['type']
+        if processing_mode == 'image_matching':
+            pass
+        elif processing_mode == 'screen_change_rate':
+            test_warm_boot_with_diff()
 
         publish_msg({'measurement': ['resume']}, 'analysis_response')
 
@@ -50,3 +34,29 @@ def test_warm_boot():
         publish_msg({'measurement': ['resume']}, error_detail, level='error')
         logger.error(f"error in test_warm_boot: {err}")
         logger.warning(error_detail)
+
+
+def test_warm_boot_with_diff():
+    args = load_input()
+
+    event_log = get_data_of_event_log(args.timestamps[0], args.timestamps[-1])
+    remocon_times = get_power_key_times(event_log)
+    logger.info(f'remocon_times: {remocon_times}')
+
+    with tempfile.TemporaryDirectory(dir='/tmp') as output_dir:
+        warm_boot_results = []
+        crop_videos = crop_video_with_opencv(args.video_path, args.timestamps, remocon_times, output_dir, get_setting_with_env('WARM_BOOT_DURATION', 10))
+        for crop_video in crop_videos:
+            if not check_poweroff_video(crop_video.video_path):
+                continue
+            result = task_boot_test_with_diff(crop_video.video_path, crop_video.timestamps, crop_video.timestamps[0])
+            logger.info(f'result: {result}')
+            warm_boot_results.append(result)
+
+    for result in warm_boot_results:
+        if result['status'] == 'success':
+            report_output(CollectionName.WARM_BOOT.value, {
+                'timestamp': get_utc_datetime(result['diff_timestamp']),
+                'measure_time': result['diff_time'],
+            })
+
