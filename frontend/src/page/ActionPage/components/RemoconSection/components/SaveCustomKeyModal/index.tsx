c696a7fc5f5b3486195fb94b2bc3df3c602fb5f2
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-08-25 11:44:09 +0900

    SaveCustomKeyModal 구현

---
+++ frontend/src/page/ActionPage/components/RemoconSection/components/SaveCustomKeyModal/index.tsx
@@ -0,0 +1,114 @@
+import Modal from '@global/ui/Modal'
+import React, { useEffect, useRef, useState } from 'react'
+import { Button, Input, Text } from '@global/ui'
+import { useMutation, useQuery } from 'react-query'
+import { getRemocon, postCustomKey } from '../../api/func'
+import { Remocon } from '../../api/entity'
+
+interface SaveCustomKeyModalProps {
+  isOpen: boolean
+  close: () => void
+  remoconInput: string[]
+  remocon: Remocon
+}
+
+const SaveCustomKeyModal: React.FC<SaveCustomKeyModalProps> = ({ isOpen, close, remoconInput, remocon }) => {
+  /**
+   * 최종적으로 추가할 new CustomKeyName
+   */
+  const [newCustomKeyName, setNewCustomKeyName] = useState<string>(remoconInput.toString())
+
+  // TODO: 이 ref를 적용시키려면 공용 컴포넌트들이 ref를 받을 수 있어야 함..
+  const firstFocusableElementRef = useRef<HTMLInputElement>(null)
+  const lastFocusableElementRef = useRef<HTMLButtonElement>(null)
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === 'Tab') {
+        // shift : 역으로 이동
+        if (event.shiftKey) {
+          if (document.activeElement === firstFocusableElementRef.current) {
+            event.preventDefault()
+            lastFocusableElementRef.current?.focus()
+          }
+        } else if (document.activeElement === lastFocusableElementRef.current) {
+          event.preventDefault()
+          firstFocusableElementRef.current?.focus()
+        }
+      }
+    }
+
+    firstFocusableElementRef.current?.focus()
+
+    document.addEventListener('keydown', handleKeyDown)
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyDown)
+    }
+  }, [])
+
+  const { refetch } = useQuery<Remocon[]>(['remocon'], () => getRemocon(), {
+    onError: (err) => {
+      console.error(err)
+    },
+  })
+
+  const { mutate: postCustomKeyMutate } = useMutation(postCustomKey, {
+    onSuccess: () => {
+      refetch()
+    },
+    onError: (err) => {
+      console.error(err)
+    },
+  })
+
+  return (
+    <Modal
+      mode="center"
+      isOpen={isOpen}
+      close={() => {
+        setNewCustomKeyName('')
+        close()
+      }}
+    >
+      <div className="w-[500px] h-[230px] flex flex-col bg-light-black rounded-[10px] p-6">
+        <Text colorScheme="light" className="!text-2xl" weight="bold">
+          Custom key
+        </Text>
+        <Input
+          className="mt-6"
+          value={newCustomKeyName}
+          onChange={(e) => setNewCustomKeyName(e.target.value)}
+          //   ref={firstFocusableElementRef}
+        />
+        <div className="flex justify-end mt-6">
+          <Button
+            colorScheme="primary"
+            className="w-[132px] h-[48px] mr-3 text-white rounded-3xl"
+            onClick={() => {
+              postCustomKeyMutate({
+                newCustomKey: { name: remoconInput.join(','), custom_code: remoconInput, remocon_name: remocon.name },
+              })
+              close()
+            }}
+          >
+            Submit
+          </Button>
+          <Button
+            colorScheme="grey"
+            className="w-[132px] h-[48px] text-white rounded-3xl"
+            // ref={lastFocusableElementRef}
+            onClick={() => {
+              setNewCustomKeyName('')
+              close()
+            }}
+          >
+            Cancel
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  )
+}
+
+export default SaveCustomKeyModal
