be518481501b06f6903f77d064e8ddb73878e120
kjune1236 <kjune1236@gmail.com>
2023-08-25 18:00:45 +0900

    add get set connection info

--- stb_log/scripts/connection/external.py
+++ stb_log/scripts/connection/external.py
@@ -1,6 +1,6 @@
 from typing import Dict
 
-from scripts.connection.redis_conn import get_value
+from scripts.connection.redis_conn import get_value, set_value, get_all
 from scripts.config.constant import RedisDB
 
 
@@ -9,3 +9,15 @@ def get_scenario_info() -> Dict:
         'scenario_id': get_value('testrun', 'scenario_id', '', db=RedisDB.hardware),
         'testrun_id': get_value('testrun', 'id', '', db=RedisDB.hardware),
     }
+
+
+def set_connection_info(host: str, port: int, username: str, password: str, connection_mode: str) -> Dict:
+    set_value('connection_info', 'host', host)
+    set_value('connection_info', 'port', port)
+    set_value('connection_info', 'username', username)
+    set_value('connection_info', 'password', password)
+    set_value('connection_info', 'connection_mode', connection_mode)
+
+
+def get_connection_info() -> Dict:
+    return get_all('connection_info')
