b5e60620e8407a2b2d13e91f90e98a1c90bfeb53
woojinjo <e05062@nextlab.co.kr>
2023-08-25 11:25:20 +0900

    update network info to independant feature - add info log - add some ip, mac get functions for its' own - enum etherstate

--- network/scripts/auto_stb_ip/find_stb_ip.py
+++ network/scripts/auto_stb_ip/find_stb_ip.py
@@ -5,10 +5,11 @@ from multiprocessing import Event, Process
 from ..configs.config import RedisDBEnum, get_value
 from ..control.network_control.command_executor import traffic_change
 from .brute_ping import brute_ping_ipv4
-from .network_info import get_ethernet_state, get_private_ip
+from ..info.network_info import get_ethernet_state, get_private_ip, EthernetState
 
 logger = logging.getLogger('main')
 TIMEOUT = 0.1
+STABLE_DELAY = 5
 UNIT_DELAY = 10
 
 
@@ -23,34 +24,34 @@ class STBIPFinder:
 
         stb_nic = get_value('network', 'stb_nic')
         private_ip = get_private_ip()
-        prev_state = 'down'
+        prev_state = EthernetState.down
         stb_ip = None
 
         while not self.stop_event.is_set():
             current_state = get_ethernet_state(stb_nic)
-            if (stb_ip is None or prev_state == 'down') and current_state == 'up':
-                logger.info('New Settop nic conection detected! wait 5 seconds for stable connection')
-                time.sleep(5)
+            if (stb_ip is None or prev_state == EthernetState.down) and current_state == EthernetState.up:
+                logger.info(f'New Settop nic conection detected! wait {STABLE_DELAY} seconds for stable connection')
+                time.sleep(STABLE_DELAY)
 
-                original = brute_ping_ipv4(private_ip, timeout=TIMEOUT)
                 original_delay = get_value('hardware_configuration', 'packet_delay', db=RedisDBEnum.hardware)
+
+                original_ip_values = {k: v for k, v in brute_ping_ipv4(private_ip, timeout=TIMEOUT)}
                 traffic_change(nic=stb_nic, delay=original_delay + UNIT_DELAY)
-                plus_fifty = brute_ping_ipv4(private_ip, timeout=TIMEOUT)
+                augmented_ip_values = {k: v for k, v in brute_ping_ipv4(private_ip, timeout=TIMEOUT)}
                 traffic_change(nic=stb_nic, delay=original_delay)
 
-                original_values = {k: v for k, v in original}
-                for ip, ping_value in plus_fifty[::-1]:
-                    if ping_value - original_values.get(ip, TIMEOUT) > (UNIT_DELAY * 0.9) / 1000:
+                for ip, ping_value in augmented_ip_values.items()[::-1]:
+                    if ping_value - original_ip_values.get(ip, TIMEOUT) > (UNIT_DELAY * 0.9) / 1000:
                         stb_ip = ip
                         logger.info(f'STB: {stb_ip}')
                         break
                 else:
                     logger.error('Failed to find STB. maybe settop is not reachable')
-                    logger.debug(f'Result: {original_values} / {plus_fifty}')
+                    logger.debug(f'Result: {original_ip_values} / {augmented_ip_values}')
 
             else:
                 time.sleep(0.5)
-            if current_state == 'down':
+            if current_state == EthernetState.down:
                 stb_ip = None
 
             prev_state = current_state
---
+++ network/scripts/info/network_info.py
@@ -0,0 +1,121 @@
+import logging
+import re
+import requests
+
+from ..control.command import get_stdout
+from ..utils._exceptions import handle_errors
+from ..utils.network import check_ipv4, check_ipv6
+
+logger = logging.getLogger('info')
+
+
+class EthernetState:
+    up: str = 'up'
+    down: str = 'down'
+
+
+@handle_errors
+def ping_google() -> bool:
+    try:
+        result = get_stdout('ping 8.8.8.8 -c 1 -w 1')
+        connected = '1 received' in result
+    except:
+        connected = False
+    return connected
+
+
+@handle_errors
+def get_ethernet_state(nic: str):
+    return get_stdout(f'cat /sys/class/net/{nic}/operstate', log=False).strip()
+
+
+@handle_errors
+def get_private_ip() -> str:
+    bridge = 'br0'
+
+    bridge_ipv4_info = get_stdout(f'ip -4 addr show {bridge}')
+
+    bridge_ipv4 = re.search(r'(?<=inet\s)\d+(\.\d+){3}', bridge_ipv4_info)
+
+    if bridge_ipv4:
+        private_ip = bridge_ipv4.group()
+    else:
+        pass  # use 0.0.0.0
+
+    return private_ip
+
+
+@handle_errors
+def get_public_ip() -> str:
+    public_ip = '0.0.0.0'
+
+    # ipv4 시도 -> 모두 실패 시 ipv6 한번 시도 -> 모두 실패 시 공백 처리
+    # 고객사 요청으로 public ip를 ipv4로 하는 것임.
+    timeout = 3
+    default_urls = ['ip.dev-nextlab.com']
+    third_party_urls = ['icanhazip.com', 'ifconfig.co', 'checkip.amazonaws.com']
+    ip_getter_urls = default_urls + third_party_urls
+    for address in ip_getter_urls:
+        ip = get_stdout(f'curl -4 -s {address} -m {timeout}').strip()
+        if check_ipv4(ip):
+            public_ip = ip
+            break
+    else:
+        ip = get_stdout(f'curl -s {ip_getter_urls[0]} -m {timeout}').strip()
+        if check_ipv6(ip):
+            public_ip = ip
+        else:
+            pass  # use 0.0.0.0
+
+    return public_ip
+
+
+@handle_errors
+def get_gateway_ips(nic: str):
+    return get_stdout(f'ip route show | grep {nic} | awk \'/default/ {{print $3}}\'').strip()
+
+
+@handle_errors
+def get_gateway_mac_address() -> str:
+    arp_info = get_stdout('arp _gateway')
+    upper_mac_re = re.search(r'ether\s+(?P<mac>.{2}:.{2}:.{2}:.{2}:.{2}:.{2})', arp_info)
+    upper_mac = upper_mac_re['mac'] if upper_mac_re else '00:00:00:00:00:00'
+
+    return upper_mac
+
+
+@handle_errors
+def get_detail_ip_info(dump_file_name: str = 'ipinfo.json') -> dict:
+    info = JsonManager(dump_file_name)
+    info.load()
+    result = info.data
+    ip = get_public_ip()
+    if ip != result.get('ip'):
+        # 1k per month requests for free.
+        result = {'ip': None, 'city': 'Seoul', 'loc': "37.5602,127.0401", 'country': 'KR', 'line': 'KT'}
+
+        logger.info('Get ip info from 3rd party program')
+        res = requests.post('http://ipinfo.io', timeout=5)
+        res_result = res.json()
+        isp = res_result['org'].lower()
+        if 'korea telecom' in isp:
+            res_result['line'] = 'KT'
+        elif 'sk broadband' in isp:
+            res_result['line'] = 'SK'
+        elif 'lg powercomm' in isp:
+            res_result['line'] = 'LG'
+        else:
+            if 'kt' in isp:
+                res_result['line'] = 'KT'
+            elif 'sk' in isp:
+                res_result['line'] = 'SK'
+            elif 'lg' in isp:
+                res_result['line'] = 'LG'
+            else:
+                res_result['line'] = 'etc'
+        result.update(res_result)
+        for key, value in result.items():
+            info.change(key, value)
+        info.save()
+    else:
+        return result
