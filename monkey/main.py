d5693e17b7f2af530ab8684043269526ca1cab80
kjune1236 <kjune1236@gmail.com>
2023-08-28 12:01:53 +0900

    add boiler plate on monkey

---
+++ monkey/main.py
@@ -0,0 +1,48 @@
+import logging
+from typing import Dict
+
+from scripts.connection.redis_conn import get_strict_redis_connection
+from scripts.connection.redis_pubsub import Subscribe
+from scripts.config.constant import RedisChannel, RedisDB
+from scripts.log_service.log_organizer import LogOrganizer
+
+
+logger = logging.getLogger('main')
+
+
+def execute(command: dict):
+    ''' 
+    start: PUBLISH command '{"msg": "monkey", "data": {"control": "start"}}'
+    stop: PUBLISH command '{"msg": "monkey", "data": {"control": "stop"}}'
+    '''
+    if command.get('msg', '') == 'monkey':
+        arg = command.get('data', {})
+        logger.info(f'msg: monkey. arg: {arg}')
+
+        control = arg.get('control', '')
+        if control == 'start':
+            start()
+        elif control == 'stop':
+            stop()
+        else:
+            logger.warning(f'Unknown control: {control}')
+
+
+def main():
+    with get_strict_redis_connection(RedisDB.hardware) as src:
+        for command in Subscribe(src, RedisChannel.command):
+            execute(command)
+
+
+if __name__ == '__main__':
+    try:
+        log_organizer = LogOrganizer(name='monkey')
+        log_organizer.set_stream_logger('main')
+        log_organizer.set_stream_logger('connection')
+        logger.info('Start monkey container')
+        
+        main()
+
+    finally:
+        logger.info('Close monkey container')
+        log_organizer.close()
