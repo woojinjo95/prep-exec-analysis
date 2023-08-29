f76cfe18c9d20f14744894267cfa60e204d1efc7
kjune1236 <kjune1236@gmail.com>
2023-08-29 17:50:31 +0900

    fix analysis manager for process term

--- analyzer/analysis_manager.py
+++ analyzer/analysis_manager.py
@@ -1,10 +1,9 @@
 import multiprocessing
 import time
 import logging
-import psutil
 import traceback
 import json
-import threading
+import uuid
 from typing import Dict, Callable, Tuple, List
 
 from scripts.format import Command
@@ -24,6 +23,7 @@ class AnalysisManager:
         logger.info(f'analysis mode: {self.mode}')
 
         self.storage = RedisStorage()
+        self.pool = multiprocessing.Pool()
 
         if self.mode == "sync":
             self.start_command_executor()
@@ -36,8 +36,10 @@ class AnalysisManager:
             logger.info(f'execute: {func.__name__}{args}')
             if self.mode == "sync":
                 self.storage.enqueue_command(func, args)
-            else:
+            elif self.mode == 'async':
                 self.start_process(func, args)
+            else:
+                logger.error(f'invalid analysis mode: {self.mode}')
 
     def parse_command(self, command: Dict) -> List[Tuple[Callable, Tuple]]:
         exec_list = []
@@ -75,7 +77,7 @@ class AnalysisManager:
                 if command:
                     func, args = command
                     proc = self.start_process(func, args)
-                    proc.join()
+                    result = proc.get()
                 else:
                     pass
 
@@ -83,20 +85,24 @@ class AnalysisManager:
         proc.start()
 
     ##### Process Control #####
-    def start_process(self, func: Callable, args: Tuple) -> multiprocessing.Process:
-        proc = multiprocessing.Process(target=func, args=args)
-        proc.start()
-        self.storage.store_process(proc.pid, {'func': func.__name__, 'args': args})
-        logger.info(f'start process: {proc.pid}')
-        return proc
+    def start_process(self, func: Callable, args: Tuple):
+        process_id = str(uuid.uuid4())
+        async_result = self.pool.apply_async(func, args=args, callback=lambda result: self.cleanup(process_id))
+        self.storage.store_process(process_id, {'func': func.__name__, 'args': args})
+        logger.info(f'started process: {process_id}')
+        return async_result  # Note: returning the async result and not the process object
+    
+    def cleanup(self, process_id):
+        self.storage.remove_process(process_id)
+        logger.info(f'removed process: {process_id}')
 
-    def stop_processes(self):
-        self.storage.empty_command_queue()
-        self.storage.terminate_all_processes()
-        logger.info('stop all processes')
+    # def stop_processes(self):
+    #     self.storage.empty_command_queue()
+    #     self.storage.terminate_all_processes()
+    #     logger.info('stop all processes')
 
-    def remove_process_by_id(self, pid):
-        self.storage.remove_process(pid)
+    # def remove_process_by_id(self, pid):
+    #     self.storage.remove_process(pid)
 
 
 class RedisStorage:
@@ -104,30 +110,28 @@ class RedisStorage:
         self.client = get_strict_redis_connection()
         self.processes_name = "processes"
         self.cmd_queue_name = "cmd_queue"
-
-        self.periodic_cleanup()
         
     ##### Process Management #####
     def store_process(self, pid: int, metadata: Dict={}):
         self.client.hset(self.processes_name, pid, json.dumps(metadata))
 
-    def terminate_all_processes(self):
-        for pid in self.get_all_pids():
-            self.remove_process(pid)
-        self.client.delete(self.processes_name)
+    # def terminate_all_processes(self):
+    #     for pid in self.get_all_pids():
+    #         self.remove_process(pid)
+    #     self.client.delete(self.processes_name)
 
     def remove_process(self, pid: int):
-        try:
-            process = psutil.Process(pid)
-            process.terminate()
-        except psutil.NoSuchProcess:
-            logger.warning(f"Process with PID {pid} not found.")
+        # try:
+        #     process = psutil.Process(pid)
+        #     process.terminate()
+        # except psutil.NoSuchProcess:
+        #     logger.warning(f"Process with PID {pid} not found.")
         self.client.hdel(self.processes_name, pid)
 
-    def get_all_pids(self) -> List[int]:
-        all_pids = self.client.hkeys(self.processes_name)
-        all_pids = [int(pid.decode('utf-8')) for pid in all_pids]
-        return all_pids
+    # def get_all_pids(self) -> List[int]:
+    #     all_pids = self.client.hkeys(self.processes_name)
+    #     all_pids = [int(pid.decode('utf-8')) for pid in all_pids]
+    #     return all_pids
 
     ##### Queue Management #####
     def enqueue_command(self, func: Callable, args: Tuple):
@@ -155,17 +159,6 @@ class RedisStorage:
     def command_length(self) -> int:
         return self.client.llen(self.cmd_queue_name)
 
-    def empty_command_queue(self):
-        while self.client.llen(self.cmd_queue_name) > 0:
-            self.client.rpop(self.cmd_queue_name)
-
-    ##### Cleanup #####
-    def periodic_cleanup(self):
-        def cleanup_terminated_processes():
-            all_pids = self.get_all_pids()
-            for pid in all_pids:
-                if not psutil.pid_exists(pid):
-                    self.client.hdel(self.processes_name, pid)
-                    logger.info(f'process {pid} is terminated. remove from storage.')
-        cleanup_terminated_processes()
-        threading.Timer(5, self.periodic_cleanup).start()
+    # def empty_command_queue(self):
+    #     while self.client.llen(self.cmd_queue_name) > 0:
+    #         self.client.rpop(self.cmd_queue_name)
