7bca4c45e01a37fac26d58732cfcf3e5df7de55c
kjune1236 <kjune1236@gmail.com>
2023-08-30 11:47:54 +0900

    add freeze main

--- analyzer/main/freeze.py
+++ analyzer/main/freeze.py
@@ -1,93 +1,60 @@
-import logging
+# FIXME 에 해당하는 부분을 수정
 
-from analysis_manager import AnalysisManager
+import logging
+from typing import Dict
+
 from scripts.connection.redis_conn import get_strict_redis_connection
 from scripts.connection.redis_pubsub import Subscribe
 from scripts.config.constant import RedisChannel, RedisDB
-from scripts.config.config import get_setting_with_env
 from scripts.log_service.log_organizer import LogOrganizer
-from scripts.
+from scripts.modules.freeze_detect import FreezeDetect
 
 
 logger = logging.getLogger('main')
 
-
-freeze_manager = None
-
-
-def start_logcat_manager(connection_info: dict):
-    global freeze_manager
-
-    if freeze_manager is not None:
-        logger.warning('LogcatManager is already alive')
-    else:
-        freeze_manager = LogcatManager(connection_info=connection_info)
-        freeze_manager.start()
-        logger.info('Start LogcatManager')
+service_name = 'freeze'  # FIXME: service name
 
 
-def stop_logcat_manager():
-    global freeze_manager
+class CommandExecutor:
+    def __init__(self):
+        self.service_module = FreezeDetect()  # FIXME: module
 
-    if freeze_manager is not None:
-        freeze_manager.stop()
-        freeze_manager = None
-        logger.info('Stop LogcatManager')
-    else:
-        logger.warning('LogcatManager is not alive')
+    def start_service_module(self):
+        self.service_module.start()
 
+    def stop_service_module(self):
+        self.service_module.stop()
 
-def command_parser(command: dict):
-    ''' 
-    start: PUBLISH command '{"msg": "stb_log", "data": {"control": "start"}}'
-    stop: PUBLISH command '{"msg": "stb_log", "data": {"control": "stop"}}'
-    connection info: PUBLISH command '{"msg": "config", "data": {"mode": "adb", "host": "192.168.30.30", "port": "5555", "username": "root", "password": ""}}'
-    '''
+    def execute(self, command: Dict):
+        # PUBLISH command '{"msg": "analysis", "data": {"measurement": ["freeze"]}}'  # FIXME: example measurement
 
-    if command.get('msg') == 'stb_log':
-        arg = command.get('data', {})
-        logger.info(f'msg: stb_log. arg: {arg}')
+        if command.get('msg') == 'analysis':
+            data = command.get('data', {})
+            logger.info(f'msg: analysis. data: {data}')
 
-        control = arg.get('control', '')
-        if control == 'start':
-            start()
-        elif control == 'stop':
-            stop()
-        else:
-            logger.warning(f'Unknown control: {control}')
-
-    if command.get('msg') == 'config':
-        data = command.get('data')
-        data['connection_mode'] = data['mode']
-        del data['mode']
-        connection_info = data
-        logger.info(f'connection_info: {connection_info}')
-        set_connection_info(**connection_info)
-
-        # restart
-        stop()
-        start()
-
+            measurement = data.get('measurement', [])
+            if 'freeze' in measurement:  # FIXME: measurement command name
+                self.start_service_module()
 
 
 
 def main():
-    analysis_manager = AnalysisManager(mode=get_setting_with_env('ANALYSIS_EXEC_MODE', 'async'))
-
+    command_executor = CommandExecutor()
     with get_strict_redis_connection(RedisDB.hardware) as src:
         for command in Subscribe(src, RedisChannel.command):
-            analysis_manager.register(command)
+            command_executor.execute(command)
 
 
 if __name__ == '__main__':
     try:
-        log_organizer = LogOrganizer(name='analyzer')
+        log_organizer = LogOrganizer(name=service_name)
         log_organizer.set_stream_logger('main')
         log_organizer.set_stream_logger('connection')
-        logger.info('Start analyzer container')
+        logger.info(f'Start {service_name} container')
         
         main()
 
     finally:
-        logger.info('Close analyzer container')
+        logger.info(f'Close {service_name} container')
         log_organizer.close()
+
