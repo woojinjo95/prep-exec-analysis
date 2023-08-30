3d59954fd1337becf026c331737bb71f27dfeb18
정보미 <jeongbomi@nextlab.co.kr>
2023-08-30 14:46:11 +0900

    설정가능한 분석유형 리스트 정의

--- frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/constant/index.ts
+++ frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/constant/index.ts
@@ -1,6 +1,17 @@
 import { AnalysisType } from '@global/constant'
 
 /**
+ * 설정 가능한 분석유형 리스트
+ */
+export const ConfigurableAnalysisTypes: (keyof typeof AnalysisType)[] = [
+  'freeze',
+  'resume',
+  'boot',
+  'log_level_finder',
+  'log_pattern_matching',
+] as const
+
+/**
  * 측정타입 라벨
  */
 export const AnalysisTypeLabel: { [key in keyof typeof AnalysisType]: string } = {
