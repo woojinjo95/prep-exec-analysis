b0a0f979a9f482057f60997745288724ccbb1bae
정보미 <jeongbomi@nextlab.co.kr>
2023-08-30 13:51:01 +0900

    eslint 경고 픽스

--- frontend/src/page/ActionPage/components/RemoconSection/components/AddCustomKeyModal/index.tsx
+++ frontend/src/page/ActionPage/components/RemoconSection/components/AddCustomKeyModal/index.tsx
@@ -87,7 +87,7 @@ const AddCustomKeyModal: React.FC<AddCustomKeyModalProps> = ({
     <>
       {!isSubmitted && (
         <Modal
-          isOpen
+          isOpen={isOpen}
           close={() => {
             setRemoconInput([])
             close()
