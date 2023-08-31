30ab9fdbf9d6d09b508366d7790fd4a974a017ef
정보미 <jeongbomi@nextlab.co.kr>
2023-08-31 14:31:18 +0900

    ColorPicker 공통 UI 일부 수정, 로그패턴 추가 모달 UI 작업(미완)

--- frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/components/SetAnalysisItemPanel/AnalysisItemList/LogPatternMatchingAnalysisItem/AddLogPatternModal.tsx
+++ frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/components/SetAnalysisItemPanel/AnalysisItemList/LogPatternMatchingAnalysisItem/AddLogPatternModal.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Modal } from '@global/ui'
+import { Button, ColorPickerBox, Input, Modal, Title } from '@global/ui'
 
 interface AddLogPatternModalProps {
   isOpen: boolean
@@ -13,8 +13,46 @@ interface AddLogPatternModalProps {
  */
 const AddLogPatternModal: React.FC<AddLogPatternModalProps> = ({ isOpen, close }) => {
   return (
-    <Modal isOpen={isOpen} close={close} title="Add Log Pattern">
-      <div>hi</div>
+    <Modal
+      isOpen={isOpen}
+      close={close}
+      title="Add Log Pattern"
+      className="w-[60vw] h-[60vh] min-h-[600px] min-w-[960px]"
+    >
+      <div className="w-full h-full grid grid-cols-[auto_1fr] grid-rows-[auto_auto_1fr_auto_auto] gap-x-10 gap-y-3">
+        <Title as="h3" colorScheme="light" className="pt-3">
+          Log Pattern Name
+        </Title>
+        <div className="flex">
+          <Input placeholder="Untitled Log Pattern" />
+        </div>
+
+        <Title as="h3" colorScheme="light" className="pt-3">
+          Log Level
+        </Title>
+        <div className="flex">
+          <Input />
+        </div>
+
+        <Title as="h3" colorScheme="light" className="pt-3">
+          Regular Expression
+        </Title>
+        <div />
+
+        <div>
+          <Title as="h3" colorScheme="light">
+            Color
+          </Title>
+        </div>
+        <div className="flex items-center">
+          <ColorPickerBox color="red" />
+        </div>
+
+        <div className="col-span-2 flex justify-end items-center gap-x-3">
+          <Button colorScheme="primary">Save</Button>
+          <Button colorScheme="grey">Cancel</Button>
+        </div>
+      </div>
     </Modal>
   )
 }
