bb4e9c6e959efcc0d319a741b4dc010303b8fe22
kjune1236 <kjune1236@gmail.com>
2023-08-31 13:21:40 +0900

    split check cursor is same

--- monkey/scripts/analysis/monkey/util.py
+++ monkey/scripts/analysis/monkey/util.py
@@ -19,17 +19,14 @@ inverse_keys = {
 }
 
 
-# 이미지 GET
 def get_current_image() -> np.ndarray:
     return get_snapshot()
 
 
-# 키 입력
 def exec_key(key: str, key_interval: float, profile: str):
     publish_remocon_msg(profile, key, sleep=key_interval)
 
 
-# 여러 키 입력
 def exec_keys(keys: List[str], *args, **kwargs):
     logger.info(f'exec_keys: {keys}')
     for key in keys:
@@ -37,28 +34,24 @@ def exec_keys(keys: List[str], *args, **kwargs):
     # time.sleep(3)
 
 
-# 이전과 현재 커서의 동일성 체크
 def check_cursor_is_same(prev_image: np.ndarray, prev_cursor: Tuple, image: np.ndarray, cursor: Tuple, 
                         iou_thld: float=0.9, min_color_depth_diff: int=10, diff_thld: float=0.05) -> bool:
-    def preprocess_image(image: np.ndarray) -> np.ndarray:
-        return cv2.cvtColor(cv2.resize(image, (960, 540)), cv2.COLOR_BGR2GRAY)
-
     logger.info(f'cursor same check. prev_cursor: {prev_cursor}, cursor: {cursor}')
     if prev_image is None or image is None or prev_cursor is None or cursor is None:
         return False
     else:
-        iou_rate = calc_iou(prev_cursor, cursor)
-        diff_rate = calc_diff_rate(preprocess_image(get_cropped_image(prev_image, prev_cursor)), 
-                                preprocess_image(get_cropped_image(image, cursor)), 
-                                min_color_depth_diff)
-        # 1. iou가 일정 이상 작음 => 커서가 움직임
-        # 2. iou가 일정 이상 크지만 diff가 일정이상 큼 => 커서는 움직이지 않았지만, 내용물이 변함
-        same = False if iou_rate < iou_thld or (iou_rate > iou_thld and diff_rate > diff_thld) else True
-        logger.info(f'same: {same}, iou_rate: {iou_rate}, iou_thld: {iou_thld:.6f}, diff_rate: {diff_rate}, diff_thld: {diff_thld:.6f}')
+        positional_similar = check_positional_similar(prev_cursor, cursor, iou_thld)
+        temporal_similar = check_temporal_similar(get_cropped_image(prev_image, prev_cursor), 
+                                        get_cropped_image(image, cursor), 
+                                        min_color_depth_diff, 
+                                        diff_thld)
+        # 1. 위치적 동일성이 일정 수준 이하 => 커서가 움직임
+        # 2. 위치적 동일성이 높지만, 시간적 동일성이 낮음 => 커서는 움직이지 않았지만, 내용물이 변함
+        same = False if not positional_similar or (positional_similar and not temporal_similar) else True
+        logger.info(f'check cursor is same. same: {same}, positional_similar: {positional_similar}, temporal_similar: {temporal_similar}')
         return same
 
 
-# 주어진 루트 최적화
 def optimize_path(path: List[str]) -> List[str]:
     stack = []
     for action in path:
@@ -69,7 +62,7 @@ def optimize_path(path: List[str]) -> List[str]:
     return stack
 
 
-# 다음 노드로 루트를 변경
+# 다음 노드로 route를 변경
 def head_to_next(key_histories: List[str], depth_key: str) -> List[str]:
     try:
         while True:
@@ -86,7 +79,22 @@ def head_to_next(key_histories: List[str], depth_key: str) -> List[str]:
         logger.warning(f'key_histories: {key_histories}')
         raise IndexError('key_histories is empty.')
 
+
+def check_positional_similar(prev_cursor: Tuple, cursor: Tuple, iou_thld: float=0.9) -> bool:
+    iou_rate = calc_iou(prev_cursor, cursor)
+    logger.info(f'check positional similar. iou_rate: {iou_rate:.6f}, iou_thld: {iou_thld:.6f}')
+    return iou_rate > iou_thld
+
+
+def check_temporal_similar(prev_image: np.ndarray, image: np.ndarray, min_color_depth_diff: int=10, diff_thld: float=0.05) -> bool:
+    def preprocess_image(image: np.ndarray) -> np.ndarray:
+        return cv2.cvtColor(cv2.resize(image, (960, 540)), cv2.COLOR_BGR2GRAY)
     
+    diff_rate = calc_diff_rate(preprocess_image(prev_image), preprocess_image(image), min_color_depth_diff)
+    logger.info(f'check temporal similar. diff_rate: {diff_rate:.6f}, diff_thld: {diff_thld:.6f}')
+    return diff_rate < diff_thld
+
+
 
 class FrameInfo:
     def __init__(self, image: np.ndarray, cursor: Tuple[int, int, int, int]):
