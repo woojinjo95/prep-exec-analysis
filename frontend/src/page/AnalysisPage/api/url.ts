8c30e3934e7a51539c1c4b0a8aacceda32906d0d
정보미 <jeongbomi@nextlab.co.kr>
2023-09-01 17:03:33 +0900

    video 관련 api - url 변경

--- frontend/src/page/AnalysisPage/api/url.ts
+++ frontend/src/page/AnalysisPage/api/url.ts
@@ -3,8 +3,8 @@ type ApiName = 'partial_video' | 'video'
 const apiUrls: {
   [key in ApiName]: string
 } = {
-  partial_video: '/api/v1/file/partial_video',
-  video: '/api/v1/file/video',
+  partial_video: '/api/v1/analysis_result/file/partial_video',
+  video: '/api/v1/analysis_result/file/video',
 }
 
 export default apiUrls
