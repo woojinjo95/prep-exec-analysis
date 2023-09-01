c23f90d9eb24190b85fd6b56e9c1d7081149fbaf
minmax <minmax@nextlab.co.kr>
2023-09-01 10:36:30 +0900

    fix log

--- shell/app/sub/log.py
+++ shell/app/sub/log.py
@@ -13,15 +13,15 @@ async def process_log_queue(queue: asyncio.Queue, conn: any, CHANNEL_NAME: str, 
     while True:
         data = await queue.get()
         timestamp = datetime.fromtimestamp(data['timestamp'])
-        data['timestamp'] = timestamp
-        _sec = data['timestamp'].second
+        # data['timestamp'] = timestamp
+        _sec = timestamp.second
         data['scenario_id'] = testinfo['scenario_id']
         data['testrun_id'] = testinfo['testrun_id']
         
         if sec != _sec and len(buffer) > 0:
             # 저장
             ret = collection.insert_one(
-                {'time': data['timestamp'], "shell_id": shell_id, 'mode': mode, 'lines': buffer})
+                {'time': timestamp, "shell_id": shell_id, 'mode': mode, 'lines': buffer})
             print(f"insert_to_mongodb: {sec} != {_sec} / {len(buffer)}: {ret.inserted_id}")
             buffer = []
             sec = _sec
