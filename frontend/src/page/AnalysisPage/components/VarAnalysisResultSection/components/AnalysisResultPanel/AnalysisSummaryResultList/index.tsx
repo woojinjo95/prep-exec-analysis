b8468eda825659d1b3de7e641e0861d5656e761a
정보미 <jeongbomi@nextlab.co.kr>
2023-09-02 15:14:13 +0900

    분셕결과 요약데이터 조회, freeze 결과 표시 등

--- frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/components/AnalysisResultPanel/AnalysisSummaryResultList/index.tsx
+++ frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/components/AnalysisResultPanel/AnalysisSummaryResultList/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { useVideoSummary } from '@global/api/hook'
 import FreezeSummaryResultItem from './FreezeSummaryResultItem'
 import LoudnessSummaryResultItem from './LoudnessSummaryResultItem'
 import ResumeSummaryResultItem from './ResumeSummaryResultItem'
@@ -6,14 +7,23 @@ import BootSummaryResultItem from './BootSummaryResultItem'
 import ChannelChangeTimeSummaryResultItem from './ChannelChangeTimeSummaryResultItem'
 import LogLevelFinderSummaryResultItem from './LogLevelFinderSummaryResultItem'
 import LogPatternMatchingSummaryResultItem from './LogPatternMatchingSummaryResultItem'
+import { useAnalysisResultSummary } from '../../../api/hook'
 
 /**
  * 분석 결과(요약 데이터) 리스트
  */
 const AnalysisSummaryResultList: React.FC = () => {
+  const { videoSummary } = useVideoSummary()
+  const { analysisResultSummary } = useAnalysisResultSummary({
+    start_time: videoSummary?.start_time!,
+    end_time: videoSummary?.end_time!,
+    enabled: !!videoSummary,
+  })
+
+  if (!analysisResultSummary) return null
   return (
     <div className="overflow-y-auto flex flex-col gap-y-1">
-      <FreezeSummaryResultItem />
+      {analysisResultSummary.freeze?.length && <FreezeSummaryResultItem results={analysisResultSummary.freeze} />}
       <LoudnessSummaryResultItem />
       <ResumeSummaryResultItem />
       <BootSummaryResultItem />
