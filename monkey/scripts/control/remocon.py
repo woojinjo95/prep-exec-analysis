972c83e8528bc6ba7dd2b0cbb9dd570c7fe1ee94
kjune1236 <kjune1236@gmail.com>
2023-09-04 11:57:09 +0900

    add type

--- monkey/scripts/control/remocon.py
+++ monkey/scripts/control/remocon.py
@@ -2,7 +2,7 @@ import time
 from scripts.connection.redis_pubsub import publish_msg
 
 
-def publish_remocon_msg(company: str, key: str, sleep: float=0, type='ir'):
+def publish_remocon_msg(company: str, key: str, sleep: float=0, type: str='ir'):
     # PUBLISH command  '{"msg": "remocon_transmit", "data": {"key": "right", "type": "ir", "press_time": 0, "name": "roku"}}'
     publish_msg({
         'key': key,
