6c78b02f47d4ee75228dee5de8a4ea57d248906a
정보미 <jeongbomi@nextlab.co.kr>
2023-09-04 19:40:32 +0900

    MemoryChart 툴팁 적용

--- frontend/src/page/AnalysisPage/components/TimelineSection/components/MemoryChart.tsx
+++ frontend/src/page/AnalysisPage/components/TimelineSection/components/MemoryChart.tsx
@@ -1,8 +1,10 @@
-import React, { useMemo } from 'react'
+import React, { useMemo, useRef } from 'react'
 import * as d3 from 'd3'
-import { AreaChart } from '@global/ui'
+import { AreaChart, TimelineTooltip, TimelineTooltipItem, Text } from '@global/ui'
 import { CHART_HEIGHT } from '@global/constant'
+import { bytesToSize } from '@global/usecase'
 import { useMemory } from '../api/hook'
+import { useTooltipEvent } from '../hook'
 
 interface MemoryChartProps {
   scaleX: Parameters<typeof AreaChart>[0]['scaleX']
@@ -15,16 +17,18 @@ interface MemoryChartProps {
  * Memory 사용률 차트
  */
 const MemoryChart: React.FC<MemoryChartProps> = ({ scaleX, startTime, endTime, dimension }) => {
+  const wrapperRef = useRef<HTMLDivElement | null>(null)
   const { memory } = useMemory({
     start_time: startTime.toISOString(),
     end_time: endTime.toISOString(),
   })
 
-  const memoryUsage = useMemo(() => {
+  const memoryData = useMemo(() => {
     if (!memory) return null
-    return memory.map(({ timestamp, memory_usage }) => ({
+    return memory.map(({ timestamp, memory_usage, ...data }) => ({
       datetime: new Date(timestamp).getTime(),
       value: Number(memory_usage),
+      ...data,
     }))
   }, [memory])
 
@@ -33,17 +37,65 @@ const MemoryChart: React.FC<MemoryChartProps> = ({ scaleX, startTime, endTime, d
     [],
   )
 
-  if (!memoryUsage) return <div />
+  const { posX, tooltipData, onMouseMove, onMouseLeave } = useTooltipEvent<NonNullable<typeof memoryData>[number]>({
+    scaleX,
+    offsetLeft: dimension?.left,
+    width: dimension?.width,
+  })
+
+  if (!memoryData) return <div />
   return (
-    <AreaChart
-      chartWidth={dimension?.width}
-      scaleX={scaleX}
-      scaleY={scaleY}
-      data={memoryUsage}
-      minValue={0}
-      strokeColor="#fa70d8"
-      fillColor="#fa70d8"
-    />
+    <div onMouseMove={onMouseMove(memoryData)} onMouseLeave={onMouseLeave} className="relative overflow-hidden">
+      {!!posX && (
+        <div
+          ref={wrapperRef}
+          className="absolute top-0 h-full w-1 bg-white opacity-30 z-10"
+          style={{
+            transform: `translateX(${posX - 2}px)`,
+          }}
+        >
+          {!!tooltipData && (
+            <TimelineTooltip posX={posX} data={tooltipData} wrapperRef={wrapperRef}>
+              <TimelineTooltipItem label="Total RAM">
+                <Text colorScheme="light">{bytesToSize(Number(tooltipData.total_ram))}</Text>
+              </TimelineTooltipItem>
+
+              <TimelineTooltipItem label="Used RAM">
+                <Text colorScheme="light">{bytesToSize(Number(tooltipData.used_ram))}</Text>
+              </TimelineTooltipItem>
+
+              <TimelineTooltipItem label="Free RAM">
+                <Text colorScheme="light">{bytesToSize(Number(tooltipData.free_ram))}</Text>
+              </TimelineTooltipItem>
+
+              <TimelineTooltipItem label="Lost RAM">
+                <Text colorScheme="light">{bytesToSize(Number(tooltipData.lost_ram))}</Text>
+              </TimelineTooltipItem>
+            </TimelineTooltip>
+          )}
+        </div>
+      )}
+
+      {/* 툴팁 데이터 위치를 표시하는 포인트 */}
+      {!!tooltipData && !!scaleX && (
+        <div
+          className="absolute -top-[3px] -left-[3px] w-[6px] h-[6px] rounded-full z-10 opacity-70 border border-white"
+          style={{
+            transform: `translate(${scaleX(new Date(tooltipData.datetime))}px, ${scaleY(tooltipData.value)}px)`,
+          }}
+        />
+      )}
+
+      <AreaChart
+        chartWidth={dimension?.width}
+        scaleX={scaleX}
+        scaleY={scaleY}
+        data={memoryData}
+        minValue={0}
+        strokeColor="#fa70d8"
+        fillColor="#fa70d8"
+      />
+    </div>
   )
 }
 
