59cce48344c0af15699c59235ed4d06866122e89
조우진 <e05062@nextlab.co.kr>
2023-09-05 19:31:03 +0900

    add active scenario loudness info

--- media/scripts/mongo_db_update.py
+++ media/scripts/mongo_db_update.py
@@ -8,12 +8,18 @@ from typing import Dict
 from .configs.config import get_value
 from .configs.constant import RedisDBEnum
 from .connection.mongo_db.create import insert_to_mongodb
+from .connection.mongo_db.update import update_loundess_to_scenario
 from .utils._multi_process import ProcessMaintainer
 from .utils._timezone import get_utc_datetime
+from .utils._exceptions import handle_errors, handle_none_return
 
 logger = logging.getLogger('mongodb')
 
 
+SCENARIO_UPDATE_WAITING_INTERVAL = 2
+
+
+@handle_errors
 def get_testrun_info() -> Dict[str, str]:
     all_testrun_info = get_value('testrun', db=RedisDBEnum.hardware)
 
@@ -24,13 +30,30 @@ def get_testrun_info() -> Dict[str, str]:
     return testrun_info
 
 
+@handle_errors
 def format_subscribed_log(subscribed_log: Dict) -> Dict:
     return {'timestamp': get_utc_datetime(subscribed_log.get('time', time.time())),
-            'M': subscribed_log.get('M', -90.0),
-            'I': subscribed_log.get('I', -90.0)}
+            'service': subscribed_log.get('service', 'Unknown'),
+            'msg': subscribed_log.get('msg', 'Unknown'),
+            'data': subscribed_log.get('data', {})}
 
 
-class InsertToMongoDB:
+@handle_none_return(int)
+@handle_errors
+def check_valid_event_log(subscribed_log: Dict) -> bool:
+    result = False
+    if subscribed_log.get('service') in ('control', 'media', 'network', 'stb_log', 'shell'):
+        result = True
+    elif subscribed_log.get('msg') in ('config'):
+        result = True
+    else:
+        # backend, replay, analysis stb-log
+        pass
+
+    return result
+
+
+class InsertLoudnessToDB:
 
     def __init__(self):
         self.log_queue = Queue()
@@ -46,17 +69,18 @@ class InsertToMongoDB:
 
     def put(self, log: dict):
         # add filter
-        # if log.get('msg') in target_msg:
-        if True:
+        if check_valid_event_log(log):
             self.log_queue.put(log)
 
     def consume(self, stop_event: Event, run_state_event: Event):
         document = None
+        loudness_stream = False
 
         while not stop_event.is_set():
             try:
                 log = self.log_queue.get(timeout=1)
                 log_time = get_utc_datetime(log.get('time', time.time()))
+                loudness_stream = True
 
                 if document is None:
                     # document가 없음
@@ -66,15 +90,23 @@ class InsertToMongoDB:
                     document['lines'].append(format_subscribed_log(log))
                 else:
                     # 이미 document가 있고 로그 초는 늘어남
-                    insert_to_mongodb('loudness', document)
+                    insert_to_mongodb('event_log', document)
                     document = self.init_document(log_time, format_subscribed_log(log))
 
             except queues.Empty:
                 if document is not None:
                     # 1초가 지나고 document가 비지 않으면 업데이트
                     logger.info(f'Timeout for 1 second and update to mongodb')
-                    insert_to_mongodb('loudness', document)
+                    insert_to_mongodb('event_log', document)
                     document = None
+
+                if loudness_stream and log_time.timestamp() + SCENARIO_UPDATE_WAITING_INTERVAL < time.time():
+                    logger.info('Timeout for update last loudness time. update to current active sceanrio info')
+                    loudness_info = {'type': 'loudness', 'timestamp': log_time}
+                    testrun_info = get_testrun_info()
+                    scenario_id = testrun_info['scenario_id']
+                    testrun_id = testrun_info['testrun_id']
+                    update_loundess_to_scenario('scenario', scenario_id, testrun_id, loudness_info)
 
             except Exception as e:
                 logger.error(f'Comsumeing log to upload mongodb failed: {e}')
