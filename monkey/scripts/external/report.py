1d1b8ca115a6003895b509c85828cab49013719f
조은비 <eunbi0322@nextlab.co.kr>
2023-09-06 16:00:19 +0900

    변수명 변경

--- monkey/scripts/external/report.py
+++ monkey/scripts/external/report.py
@@ -30,9 +30,9 @@ def report_data(col_name: str, data: Dict):
 
 def report_section(start_time: float, end_time: float, analysis_type: str, section_id: int, image: np.ndarray, smart_sense_times: int):
     if image is not None:
-        file_name = get_utc_datetime(time.time()).strftime('%y-%m-%d %H:%M:%S')
-        image_path = save_image(file_name, image)
-        insert_to_mongodb('file', {'id': str(uuid4()), "name": f'{file_name}.png', "path": image_path})
+        image_name = get_utc_datetime(time.time()).strftime('%y-%m-%d %H:%M:%S')
+        image_path = save_image(image_name, image)
+        insert_to_mongodb('file', {'id': str(uuid4()), "name": f'{image_name}.png', "path": image_path})
     else:
         image_path = ''
 
