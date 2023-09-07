a8fb232b9ae49b4c3fc84671015cc70c0e538af9
정보미 <jeongbomi@nextlab.co.kr>
2023-09-07 20:52:06 +0900

    eslint 경고 픽스

--- frontend/src/page/ActionPage/components/RemoconSection/components/RemoconComponent/index.tsx
+++ frontend/src/page/ActionPage/components/RemoconSection/components/RemoconComponent/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useMemo, useRef, useState } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import classNames from 'classnames/bind'
 
 // import { ReactComponent as AddIcon } from '@assets/images/add.svg'
