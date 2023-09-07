5f7e20a118e2b6eb647e4080547ab803b6cc2ff5
정보미 <jeongbomi@nextlab.co.kr>
2023-09-07 16:33:22 +0900

    CardModal 및 SimpleButton 공통 UI 컴포넌트 추가

---
+++ frontend/src/global/ui/CardModal/index.tsx
@@ -0,0 +1,43 @@
+import React from 'react'
+import cx from 'classnames'
+import { ReactComponent as CloseIcon } from '@assets/images/x.svg'
+import { Title, Text, SimpleButton } from '@global/ui'
+
+interface CardModalProps {
+  isOpen: boolean
+  onClose: () => void
+  title: string
+  subtitle: string
+  className?: string
+  children: React.ReactNode | React.ReactNode[]
+}
+
+/**
+ * 원본데이터를 표시할 때 사용하는 카드 모달
+ */
+const CardModal: React.FC<CardModalProps> = ({ isOpen, onClose, title, subtitle, className, children }) => {
+  if (!isOpen) return null
+  return (
+    <div
+      className={cx(
+        'z-20 fixed bottom-0 left-0 h-[36vh] w-[calc(65vw+1px)] py-3 px-6 bg-light-black border border-charcoal rounded-t-[10px]',
+        className,
+      )}
+    >
+      <div className="pb-2 flex items-center gap-x-5">
+        <Title as="h3" colorScheme="light">
+          {title}
+        </Title>
+        <Text weight="medium">{subtitle}</Text>
+
+        <SimpleButton isIcon colorScheme="charcoal" onClick={onClose}>
+          <CloseIcon className="fill-white w-3 h-3" />
+        </SimpleButton>
+      </div>
+
+      <div>{children}</div>
+    </div>
+  )
+}
+
+export default CardModal
---
+++ frontend/src/global/ui/SimpleButton/index.tsx
@@ -0,0 +1,45 @@
+import React from 'react'
+import cx from 'classnames'
+
+interface SimpleButtonProps
+  extends React.DetailedHTMLProps<React.ButtonHTMLAttributes<HTMLButtonElement>, HTMLButtonElement> {
+  isIcon?: boolean
+  children: React.ReactNode | React.ReactNode[]
+  colorScheme?: 'dark' | 'charcoal' | 'light'
+}
+
+/**
+ * 간단한 버튼 컴포넌트
+ *
+ * @param isIcon 버튼안에 아이콘만 들어가는 경우. 아이콘 크기는 12px * 12px 권장
+ */
+const SimpleButton: React.FC<SimpleButtonProps> = ({
+  isIcon = false,
+  colorScheme = 'dark',
+  className,
+  children,
+  ...props
+}) => {
+  return (
+    <button
+      type="button"
+      className={cx(
+        'ml-auto rounded-md transition-colors flex items-center gap-x-3',
+        {
+          'py-1 px-3': !isIcon,
+          'p-2': isIcon,
+
+          'hover:bg-light-black': colorScheme === 'dark',
+          'hover:bg-charcoal': colorScheme === 'charcoal',
+          'hover:bg-[#F1F2F4]': colorScheme === 'light',
+        },
+        className,
+      )}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+}
+
+export default SimpleButton
