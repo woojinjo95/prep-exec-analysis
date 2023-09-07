ce1795b162785c2a35d55bddecba34826f100c1f
조우진 <e05062@nextlab.co.kr>
2023-09-07 16:08:50 +0900

    add full_frame snapshot - add full_frame snapshot - change filename for backend

--- media/scripts/media_process/video_snapshot.py
+++ media/scripts/media_process/video_snapshot.py
@@ -1,7 +1,7 @@
 import json
 import logging
 import os
-from uuid import uuid4
+import time
 
 import cv2
 
@@ -9,13 +9,19 @@ from ..configs.config import get_value
 from ..configs.constant import RedisChannel, RedisDBEnum
 from ..connection.mongo_db.create import insert_to_mongodb
 from ..connection.redis_pubsub import get_strict_redis_connection, publish
-from ..utils._timezone import timestamp_to_datetime_with_timezone_str
+from ..utils._timezone import (get_utc_datetime,
+                               timestamp_to_datetime_with_timezone_str)
 
 logger = logging.getLogger('snapshot')
 file_logger = logging.getLogger('file')
 
+IMAGE_EXTENSION = 'webp'
+WEBP_QUALITY = 25
+FRAME_SNAPSHOTS_IMAGE_SIZE = (160, 90)
+WEBP_PARAMS = [cv2.IMWRITE_WEBP_QUALITY, WEBP_QUALITY]
 
-def save_video_snapshot(testrun_id: str = None, video_path: str = None, relative_time: float = None):
+
+def save_video_frame_snapshot(testrun_id: str = None, video_path: str = None, relative_time: float = None):
     workspace_info = get_value('testrun', db=RedisDBEnum.hardware)
     if testrun_id is None:
         testrun_id = workspace_info['id']
@@ -74,7 +80,7 @@ def save_video_snapshot(testrun_id: str = None, video_path: str = None, relative
         metadata = {'idx': target_idx, 'total_idx': total_frame_num, 'timestamp': timestamp_to_datetime_with_timezone_str(timestamps[target_idx])}
         logger.info(f'index {target_idx} / {total_frame_num}, abs time: {timestamps[target_idx]} / {timestamp_to_datetime_with_timezone_str(timestamps[target_idx])}')
 
-        time_info = timestamp_to_datetime_with_timezone_str(format="%Y-%m-%dT%H%M%SF%f%z")
+        time_info = timestamp_to_datetime_with_timezone_str().replace('+00:00', 'Z')  # UTC+0 에서만 Z로 변경
         image_name = f'image_{time_info}.webp'
 
         ret, image = cap.read()
@@ -95,3 +101,82 @@ def save_video_snapshot(testrun_id: str = None, video_path: str = None, relative
             publish(redis_connection, RedisChannel.command, {'msg': 'video_frame_snapshot_response',
                                                              'level': log_level,
                                                              'data':  {'path': image_path, 'log': log, 'metadata': metadata}})
+
+
+def save_full_frame_video_snapshots(scenario_id: str, testrun_id: str, video_path: str = None):
+
+    output_path = os.path.join(os.path.dirname(video_path), 'frames')
+    os.makedirs(output_path, exist_ok=True)
+
+    logger.info(f'make snapshot image for {video_path}')
+
+    log = ''
+    log_level = 'info'
+
+    try:
+        cap = cv2.VideoCapture(video_path)
+        total_frame_num = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+        fps = int(cap.get(cv2.CAP_PROP_FPS))
+
+        stat_path = f'{video_path}_stat'
+        try:
+            with open(stat_path, 'r') as f:
+                json_data = json.load(f)
+
+            timestamps = json_data['data']['timestamps']
+
+        except:
+            logger.warning('stat file is not exist. just calculate video')
+            fps = cap.get(cv2.CAP_PROP_FPS)
+            timestamps = [i / fps for i in range(total_frame_num)]
+
+        if total_frame_num <= 180 * fps:  # 3 minutes
+            snapshot_count = 60
+        else:
+            # if fps is 60, snapshot count in 180 s is 60.
+            snapshot_count = total_frame_num // fps
+
+        unit_step = total_frame_num / snapshot_count  # this value is floating
+        snapshot_indices = [int(idx * unit_step) for idx in range(snapshot_count)]
+
+        image_names = []
+        for snapshot_index in snapshot_indices:
+            cap.set(cv2.CAP_PROP_POS_FRAMES, snapshot_index)
+            ret, image = cap.read()
+            if not ret:
+                log += 'Failed to get images'
+                break
+            time_info = timestamp_to_datetime_with_timezone_str(timestamps[snapshot_index]).replace('+00:00', 'Z')  # UTC+0 에서만 Z로 변경
+            image_name = f'{time_info}.{IMAGE_EXTENSION}'
+            image_names.append(image_name)
+            image_path = os.path.join(output_path, image_name)
+            save_image = cv2.resize(image, FRAME_SNAPSHOTS_IMAGE_SIZE, interpolation=cv2.INTER_AREA)
+            cv2.imwrite(image_path, save_image, WEBP_PARAMS)
+        else:
+            log += 'Succesfully images made'
+
+        document = {'timestamp': get_utc_datetime(log.get('time', time.time())),
+                    'scenario_id': scenario_id,
+                    'testrun_id': testrun_id,
+                    'video_path': video_path,
+                    'path': output_path,
+                    'extension': IMAGE_EXTENSION,
+                    'names': image_names,
+                    }
+
+        insert_to_mongodb('video_snapshots', document)
+
+    except Exception as e:
+        log += f'\n Error: {e}'
+        log_level = 'error'
+    finally:
+        with get_strict_redis_connection() as redis_connection:
+            publish(redis_connection, RedisChannel.command, {'msg': 'video_snapshots_response',
+                                                             'level': log_level,
+                                                             'data':  {'video_path': video_path,
+                                                                       'path': output_path,
+                                                                       'scenario_id': scenario_id,
+                                                                       'testrun_id': testrun_id,
+                                                                       'log': log,
+                                                                       'count': snapshot_count,
+                                                                       }})
