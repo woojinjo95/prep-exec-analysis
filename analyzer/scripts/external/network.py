8914c16ffc5f817fc34890a79cce198623855d41
kjune1236 <kjune1236@gmail.com>
2023-09-07 16:28:51 +0900

    add get_data_of_network_log

---
+++ analyzer/scripts/external/network.py
@@ -0,0 +1,27 @@
+import logging
+
+from scripts.connection.mongo_db.crud import aggregate_from_mongodb
+from scripts.external.scenario import get_scenario_info
+from scripts.util._timezone import get_utc_datetime
+
+logger = logging.getLogger('main')
+
+
+def get_data_of_network_log(start_time: float, end_time: float):
+    start_time = get_utc_datetime(start_time)
+    end_time = get_utc_datetime(end_time)
+
+    scenario_info = get_scenario_info()
+    scenario_id = scenario_info['scenario_id']
+    logger.info(f'get_data_of_network_log. scenario_id: {scenario_id}, start_time: {start_time}, end_time: {end_time}')
+    
+    pipeline = [
+        {'$match': {'scenario_id': scenario_id,
+                    'timestamp': {'$gte': start_time,
+                                  '$lte': end_time}}},
+        {'$project': {'_id': 0, 'lines': 1}},
+        {'$unwind': {'path': '$lines'}},
+        {'$replaceRoot': {'newRoot': '$lines'}}
+    ]
+    event_log = aggregate_from_mongodb(col='network_trace', pipeline=pipeline)
+    return {"items": event_log}
