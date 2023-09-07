ff010ef705f3fcd93974056a8eb644ff51d3c63e
kjune1236 <kjune1236@gmail.com>
2023-09-07 17:46:09 +0900

    move format

--- analyzer/scripts/format.py
+++ analyzer/scripts/format.py
@@ -9,6 +9,18 @@ class VideoData:
     stat_path: str
 
 
+@dataclass
+class InputData:
+    video_path: str
+    timestamps: List[float]
+
+
+@dataclass
+class CroppedInfo:
+    video_path: str
+    timestamps: List[float]
+
+
 class ReportName(Enum):
     COLOR_REFERENCE = 'color_reference'
     FREEZE = 'freeze'
@@ -23,18 +35,6 @@ class CollectionName(Enum):
     WARM_BOOT = 'an_warm_boot'
     COLD_BOOT = 'an_cold_boot'
     LOG_PATTERN = 'an_log_pattern'
-
-
-@dataclass
-class InputData:
-    video_path: str
-    timestamps: List[float]
-
-
-@dataclass
-class CroppedInfo:
-    video_path: str
-    timestamps: List[float]
 
     
 # command for subscriber
