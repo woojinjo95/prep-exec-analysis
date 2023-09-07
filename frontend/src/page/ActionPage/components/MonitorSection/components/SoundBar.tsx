65ccc70423200b518824969deaed01a3dcb0d768
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-09-07 17:39:05 +0900

    soundBar bg-black 제거

--- frontend/src/page/ActionPage/components/MonitorSection/components/SoundBar.tsx
+++ frontend/src/page/ActionPage/components/MonitorSection/components/SoundBar.tsx
@@ -9,7 +9,7 @@ const SoundBar: React.FC<SoundBarProps> = ({ value }) => {
   return (
     <div className="flex flex-col justify-between relative h-full">
       <div
-        className="absolute top-0 left-0 w-full bg-black z-10 transition-all ease-linear duration-500"
+        className="absolute top-0 left-0 w-full bg-transparent z-10 transition-all ease-linear duration-500"
         style={{
           height: `${((Math.abs(value) > 70 ? 70 : Math.abs(value)) / 70) * 100}%`,
         }}
