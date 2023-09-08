a8f1da37daf31a224d7177639cf8ccab88d5e982
kjune1236 <kjune1236@gmail.com>
2023-09-08 11:52:42 +0900

    remove useless

--- analyzer/scripts/util/video.py
+++ analyzer/scripts/util/video.py
@@ -8,19 +8,6 @@ from scripts.format import Videoinfo
 
 logger = logging.getLogger('main')
 
-class VideoCaptureContext:
-    def __init__(self, *args, **kwargs):
-        self.cap = cv2.VideoCapture(*args, **kwargs)
-        self.fps = self.cap.get(cv2.CAP_PROP_FPS)
-        self.frame_count = int(self.cap.get(cv2.CAP_PROP_FRAME_COUNT))
-
-    def __enter__(self):
-        return self
-
-    def __exit__(self, exc_type, exc_value, traceback):
-        self.cap.release()
-        cv2.destroyAllWindows()
-
 
 def FrameGenerator(video_path: str, timestamps: list = None):
     cap = cv2.VideoCapture(video_path)
