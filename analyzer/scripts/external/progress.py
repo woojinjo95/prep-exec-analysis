1ff70d4ae12726df23a093c1d9fc7a0e90cbada2
kjune1236 <kjune1236@gmail.com>
2023-09-08 14:49:56 +0900

    add ProgressManager

---
+++ analyzer/scripts/external/progress.py
@@ -0,0 +1,31 @@
+import logging
+import time
+from scripts.format import Command
+
+
+logger = logging.getLogger('main')
+
+
+class ProgressManager:
+    def __init__(self, command: Command):
+        self.start_time = time.time()
+        self.command = command
+        self.progress = 0
+        self.remaining_time = 0
+        self.update_progress(0)
+
+    def __del__(self):
+        self.update_progress(1)
+
+    def calculate_progress(self):
+        current_time = time.time()
+        elapsed_time = current_time - self.start_time
+        try:
+            self.remaining_time = elapsed_time / self.progress - elapsed_time
+        except ZeroDivisionError:
+            self.remaining_time = 0
+        logger.info(f'Progress: {self.progress:.2%} | Remaining time: {self.remaining_time:.2f}s')
+
+    def update_progress(self, progress: float):
+        self.progress = progress
+        self.calculate_progress()
