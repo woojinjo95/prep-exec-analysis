4a9441712e89c118e0db8dd6f2c8809d0205667e
kjune1236 <kjune1236@gmail.com>
2023-09-08 14:51:00 +0900

    add task_color_reference

--- analyzer/scripts/processor/color_reference.py
+++ analyzer/scripts/processor/color_reference.py
@@ -9,7 +9,8 @@ from scripts.connection.redis_pubsub import publish_msg
 from scripts.external.data import load_input
 from scripts.external.report import report_output
 from scripts.external.analysis import set_analysis_info
-from scripts.format import Command, ReportName
+from scripts.external.progress import ProgressManager
+from scripts.format import Command, ReportName, VideoInfo
 from scripts.util._timezone import get_utc_datetime
 from scripts.util.decorator import log_decorator
 from scripts.util.video import FrameGenerator
@@ -23,15 +24,7 @@ def test_color_reference():
         args = load_input()
         config = get_config()
         
-        for idx, (frame, cur_time) in enumerate(FrameGenerator(args.video_path, args.timestamps)):
-            if idx % config['skip_frame'] == 0:
-                frame = cv2.resize(frame, config['resolution'])
-                color_entropy = calc_color_entropy(frame)
-                logger.info(f"idx: {idx}, color_entropy: {color_entropy}")
-                report_output(ReportName.COLOR_REFERENCE.value, {
-                    'timestamp': get_utc_datetime(cur_time),
-                    'color_reference': color_entropy,
-                }) 
+        task_color_reference(args, config)
 
         publish_msg({'measurement': Command.COLOR_REFERENCE.value}, 'analysis_response')
         set_analysis_info(Command.COLOR_REFERENCE.value)
@@ -41,6 +34,20 @@ def test_color_reference():
         publish_msg({'measurement': Command.COLOR_REFERENCE.value, 'log': error_detail}, 'analysis_response', level='error')
         logger.error(f"error in test_color_reference: {err}")
         logger.warning(error_detail)
+
+
+def task_color_reference(args: VideoInfo, config: Dict):
+    progress_manager = ProgressManager(Command.COLOR_REFERENCE.value)
+    for idx, (frame, cur_time) in enumerate(FrameGenerator(args.video_path, args.timestamps)):
+        if idx % config['skip_frame'] == 0:
+            frame = cv2.resize(frame, config['resolution'])
+            color_entropy = calc_color_entropy(frame)
+            logger.info(f"idx: {idx}, color_entropy: {color_entropy}")
+            report_output(ReportName.COLOR_REFERENCE.value, {
+                'timestamp': get_utc_datetime(cur_time),
+                'color_reference': color_entropy,
+            }) 
+            progress_manager.update_progress(idx / args.frame_count)
 
 
 def get_config() -> Dict:
