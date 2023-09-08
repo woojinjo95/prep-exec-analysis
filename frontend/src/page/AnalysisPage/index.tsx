cd2273d333daad6046a5282daefc7b8dc29be4b1
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-09-08 11:14:59 +0900

    미사용 코드 제거

--- frontend/src/page/AnalysisPage/index.tsx
+++ frontend/src/page/AnalysisPage/index.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from 'react'
+import React, { useEffect } from 'react'
 import { useRecoilState, useRecoilValue, useSetRecoilState } from 'recoil'
 import { PageContainer, Text } from '@global/ui'
 import { useVideoSummary } from '@global/api/hook'
