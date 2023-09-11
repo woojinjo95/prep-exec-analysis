6c613dab80d8c59a4b1acb60eeb3f92ee2cf0147
조우진 <e05062@nextlab.co.kr>
2023-09-11 13:13:43 +0900

    mute command log for updating lcd. - but some feature can controllable by command message. - bt only btw.

--- control/scripts/services/machine_state.py
+++ control/scripts/services/machine_state.py
@@ -8,7 +8,9 @@ from typing import Dict
 from ..configs.config import get_value
 from ..configs.constant import RedisChannel
 from ..connection.redis_pubsub import get_strict_redis_connection, publish
+from ..device.remocon.remocon_process import RemoconProcess
 from ..state.machine_state import (get_cpu_usage_average_in_percent,
+                                   get_current_running_state,
                                    get_disk_usage_in_percent,
                                    get_machine_dut_lan_ip,
                                    get_machine_private_ip,
@@ -20,15 +22,20 @@ logger = logging.getLogger('service')
 
 HARDWARE_CONFIG = 'hardware_configuration'
 DUT_HDMI = 'enable_hdmi'
+LCD = 'lcd'
+
+
+def uptime() -> str:
+    return str(round(time.monotonic()))
 
 
 def get_machine_state() -> Dict:
     # key itself is function name of LCDString class
     # value is string or string() object
-    values = {'uptime': str(round(time.monotonic())),
-              'ir_state': 'on' if get_value(HARDWARE_CONFIG, 'remote_control_type') == 'ir' else 'off',
-              'bt_state': 'on',
-              'set_status': 'Ready',
+    values = {'ir_state': 'on' if get_value(HARDWARE_CONFIG, 'remote_control_type') == 'ir' else 'off',
+              'bt_state': 'off',
+              'uptime': uptime(),
+              'set_status': get_current_running_state(),
               'wan_ip': get_machine_private_ip(),
               'stb_ip': get_machine_dut_lan_ip(),
               'video_input_state': 'on' if get_value(HARDWARE_CONFIG, DUT_HDMI) else 'off',  # temp
@@ -41,12 +48,12 @@ def get_machine_state() -> Dict:
     return values
 
 
-def start_state_service(interval: float = 10) -> ProcessMaintainer:
+def start_state_service(remocon_process: RemoconProcess, interval: int = 10) -> ProcessMaintainer:
     logger.info('Start state service')
 
     prev_state = defaultdict(str)
 
-    def state_service(interval: float, stop_event: Event, run_state_event: Event):
+    def state_service(interval: int, stop_event: Event, run_state_event: Event):
         try:
             with get_strict_redis_connection() as src:
                 while not stop_event.is_set():
@@ -54,12 +61,20 @@ def start_state_service(interval: float = 10) -> ProcessMaintainer:
                     logger.debug(current_state)
                     for element, value in current_state.items():
                         if prev_state[element] != value:
-                            publish(src, RedisChannel.command, {'msg': 'lcd_control',
-                                                                'data': {'func_arg': f'{element}:{value}'}})
+                            lcd_string = f'{element}:{value}'
+                            remocon_process.put_command(key=lcd_string, _type=LCD)
+                            # too many log in watcher, directly input data, log is in control.
+                            # publish(src, RedisChannel.command, {'msg': 'lcd_control',
+                            #                                     'data': {'func_arg': lcd_string}})
 
                     prev_state.update(current_state)
 
-                    time.sleep(interval)
+                    time.sleep(1)
+                    for _ in range(interval - 1):
+                        # update is update by 1 seconds
+                        remocon_process.put_command(key=f'update:{uptime()}', _type=LCD)
+                        time.sleep(1)
+
         except Exception as e:
             logger.error(e)
             logger.info(traceback.format_exc())
