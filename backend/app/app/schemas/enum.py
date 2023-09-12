3b2ae9822a93cab43d3e57f1d0600d57ce7ad91f
조은비 <eunbi0322@nextlab.co.kr>
2023-09-12 15:01:05 +0900

    network_emulation 타입 추가

--- backend/app/app/schemas/enum.py
+++ backend/app/app/schemas/enum.py
@@ -73,6 +73,7 @@ class BlockTypeEnum(ExtendedEnum):
     packet_control = "packet_control"
     packet_block = "packet_block"
     device_info = "device_info"
+    network_emulation = "network_emulation"
     shell = "shell"
     monkey_test = "monkey_test"
     intelligent_monkey_test = "intelligent_monkey_test"
