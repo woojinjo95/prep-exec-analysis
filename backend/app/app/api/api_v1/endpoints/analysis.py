1eddec03bcdec87c7494d0d7cebcef0b2547bcf9
조은비 <eunbi0322@nextlab.co.kr>
2023-09-12 17:34:41 +0900

    타겟 삭제

--- backend/app/app/api/api_v1/endpoints/analysis.py
+++ backend/app/app/api/api_v1/endpoints/analysis.py
@@ -3,7 +3,7 @@ import traceback
 
 from app import schemas
 from app.api.utility import set_redis_pub_msg
-from app.crud.base import delete_many_to_mongodb
+from app.crud.base import delete_many_to_mongodb, update_to_mongodb
 from app.db.redis_session import RedisClient
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
@@ -26,13 +26,17 @@ async def start_analysis(
         }
         analysis_in = jsonable_encoder(analysis_in)
         measurements = analysis_in['measurement']
+        update_to_mongodb(col="scenario",
+                          param={"id": analysis_in['scenario_id'],
+                                 "testruns.id": analysis_in['testrun_id']},
+                          data={"testruns.$.measure_targets": []})
         for measurement in measurements:
             collection = collection_dict.get(measurement, None)
             if collection:
                 delete_many_to_mongodb(collection, {'scenario_id': analysis_in['scenario_id'],
                                                     'testrun_id': analysis_in['testrun_id']})
 
-        RedisClient.publish('command', set_redis_pub_msg(msg="start_analysis"))
+        RedisClient.publish('command', set_redis_pub_msg(msg="start_analysis"))  # TODO 기존에 쌓인 분석이 있으면 pass
     except Exception as e:
         logger.error(traceback.format_exc())
         raise HTTPException(status_code=500, detail=traceback.format_exc())
