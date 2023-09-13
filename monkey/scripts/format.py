15fec91b2c2211674926459cb4f68f72b7fd0a0c
김준한 <kjune1236@gmail.com>
2023-09-13 12:30:25 +0900

    fix section data format

--- monkey/scripts/format.py
+++ monkey/scripts/format.py
@@ -1,11 +1,15 @@
 from dataclasses import dataclass
+from datetime import datetime
+from typing import Dict
 
 
 @dataclass
 class SectionData:
-    start_time: float
-    end_time: float
-    analysis_type: str
-    section_id: int
-    image_path: str
-    smart_sense_times: int
+    start_time: datetime = None
+    end_time: datetime = None
+    analysis_type: str = ''
+    section_id: int = 0
+    image_path: str = ''
+    smart_sense_times: int = 0
+    user_config: Dict = {}
+
