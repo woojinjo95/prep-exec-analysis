74f3fa372abe17f296eb8d5594610599bee54f4c
vnslt0152@nextlab.co.kr <vnslt0152@nextlab.co.kr>
2023-09-13 13:51:46 +0900

    HLSPlayer err 대응 및 주기적 재생시간 조정

--- frontend/src/page/ActionPage/components/MonitorSection/components/HLSPlayer.tsx
+++ frontend/src/page/ActionPage/components/MonitorSection/components/HLSPlayer.tsx
@@ -1,12 +1,42 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useState } from 'react'
 import Hls from 'hls.js'
 
 export interface HlsPlayerProps extends React.VideoHTMLAttributes<HTMLVideoElement> {
   src: string
 }
 
+const pullCurrentPlayTime = (videoElement: HTMLVideoElement | null, diffTime = 0) => {
+  if (videoElement && videoElement.currentTime) {
+    // 불러온 영상의 마지막 시간 1초전으로 현재 재생시간을 맞춤
+    // eslint-disable-next-line no-param-reassign
+    videoElement.currentTime = videoElement.duration + diffTime
+    videoElement.play().catch(() => {
+      console.log('error')
+    })
+  }
+}
+
 const HLSPlayer: React.FC<HlsPlayerProps> = ({ src, autoPlay, ...props }) => {
   const playerRef = React.useRef<HTMLVideoElement | null>(null)
+  const [isConnectHLS, setIsConnectHLS] = useState<boolean>(false)
+
+  useEffect(() => {
+    window.addEventListener('focus', () => {
+      pullCurrentPlayTime(playerRef.current)
+    })
+  }, [])
+
+  useEffect(() => {
+    const timerId = setInterval(() => {
+      if (isConnectHLS) {
+        pullCurrentPlayTime(playerRef.current, -1)
+      }
+    }, 1000 * 60)
+
+    return () => {
+      clearInterval(timerId)
+    }
+  }, [isConnectHLS])
 
   useEffect(() => {
     let hls: Hls
@@ -35,7 +65,16 @@ const HLSPlayer: React.FC<HlsPlayerProps> = ({ src, autoPlay, ...props }) => {
         newHls.on(Hls.Events.MANIFEST_PARSED, () => {
           if (autoPlay && playerRef.current) {
             playerRef.current.muted = true
-            playerRef.current.play().catch((err) => console.log(err))
+            playerRef.current
+              .play()
+              .then(() => {
+                setIsConnectHLS(true)
+                pullCurrentPlayTime(playerRef.current)
+              })
+              .catch((err) => {
+                setIsConnectHLS(false)
+                console.log(err)
+              })
           }
         })
       })
@@ -50,7 +89,10 @@ const HLSPlayer: React.FC<HlsPlayerProps> = ({ src, autoPlay, ...props }) => {
               newHls.recoverMediaError()
               break
             default:
-              initPlayer()
+              // initPlayer 재 실행
+              newHls.destroy()
+
+              setTimeout(initPlayer, 2000)
               break
           }
         }
