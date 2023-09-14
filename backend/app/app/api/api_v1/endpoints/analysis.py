0512d70c0cc828f5426a6f51bb2dd00f7e66c41e
조은비 <eunbi0322@nextlab.co.kr>
2023-09-14 13:44:31 +0900

    #

--- backend/app/app/api/api_v1/endpoints/analysis.py
+++ backend/app/app/api/api_v1/endpoints/analysis.py
@@ -2,7 +2,7 @@ import logging
 import traceback
 
 from app import schemas
-from app.api.utility import set_redis_pub_msg
+from app.api.utility import get_config_from_scenario_mongodb, set_redis_pub_msg
 from app.crud.base import delete_many_to_mongodb, update_to_mongodb
 from app.db.redis_session import RedisClient
 from fastapi import APIRouter, HTTPException
@@ -25,19 +25,25 @@ async def start_analysis(
             "log_pattern_matching": "an_log_pattern"
         }
         analysis_in = jsonable_encoder(analysis_in)
-        measurements = analysis_in['measurement']
+        new_measurements = set(analysis_in['measurement'])
+        testrun_config = get_config_from_scenario_mongodb(analysis_in['scenario_id'], analysis_in['testrun_id'])
+        old_measurements = set([key for key, value in testrun_config.get('config', {}).items() if value is not None])
+
+        # measure_targets 초기화
         update_to_mongodb(col="scenario",
                           param={"id": analysis_in['scenario_id'],
                                  "testruns.id": analysis_in['testrun_id']},
                           data={"testruns.$.measure_targets": []})
-        for measurement in measurements:
+
+        for measurement in list(old_measurements - new_measurements):
             collection = collection_dict.get(measurement, None)
             if collection:
                 delete_many_to_mongodb(collection, {'scenario_id': analysis_in['scenario_id'],
                                                     'testrun_id': analysis_in['testrun_id']})
 
-        RedisClient.publish('command', set_redis_pub_msg(msg="start_analysis"))  # TODO 기존에 쌓인 분석이 있으면 pass
+        RedisClient.publish('command', set_redis_pub_msg(msg="start_analysis"))
+
     except Exception as e:
         logger.error(traceback.format_exc())
         raise HTTPException(status_code=500, detail=traceback.format_exc())
-    return {'msg': f'Start analysis: {measurements}'}
+    return {'msg': f'Start analysis: {analysis_in["measurement"]}'}
