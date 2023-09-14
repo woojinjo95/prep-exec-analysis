f5f3ec91250f0411881a96d959c9ccab38d078d0
조은비 <eunbi0322@nextlab.co.kr>
2023-09-14 14:07:28 +0900

    measure_targets 업데이트

--- backend/app/app/api/api_v1/endpoints/analysis.py
+++ backend/app/app/api/api_v1/endpoints/analysis.py
@@ -3,7 +3,7 @@ import traceback
 
 from app import schemas
 from app.api.utility import get_config_from_scenario_mongodb, set_redis_pub_msg
-from app.crud.base import delete_many_to_mongodb, update_to_mongodb
+from app.crud.base import delete_part_to_mongodb
 from app.db.redis_session import RedisClient
 from fastapi import APIRouter, HTTPException
 from fastapi.encoders import jsonable_encoder
@@ -18,31 +18,19 @@ async def start_analysis(
     analysis_in: schemas.Analysis,
 ) -> schemas.Msg:
     try:
-        collection_dict = {
-            "freeze": "an_freeze",
-            "resume": "an_warm_boot",
-            "boot": "an_cold_boot",
-            "log_pattern_matching": "an_log_pattern"
-        }
         analysis_in = jsonable_encoder(analysis_in)
         new_measurements = set(analysis_in['measurement'])
-        testrun_config = get_config_from_scenario_mongodb(analysis_in['scenario_id'], analysis_in['testrun_id'])
-        old_measurements = set([key for key, value in testrun_config.get('config', {}).items() if value is not None])
+        testrun_config = get_config_from_scenario_mongodb(
+            analysis_in['scenario_id'], analysis_in['testrun_id']).get('config', {})
+        old_measurements = set([key for key, value in testrun_config.items() if value is not None])
 
-        # measure_targets 초기화
-        update_to_mongodb(col="scenario",
-                          param={"id": analysis_in['scenario_id'],
-                                 "testruns.id": analysis_in['testrun_id']},
-                          data={"testruns.$.measure_targets": []})
-
-        for measurement in list(old_measurements - new_measurements):
-            collection = collection_dict.get(measurement, None)
-            if collection:
-                delete_many_to_mongodb(collection, {'scenario_id': analysis_in['scenario_id'],
-                                                    'testrun_id': analysis_in['testrun_id']})
+        for _type in list(old_measurements - new_measurements):
+            delete_part_to_mongodb(col="scenario",
+                                   param={"id": analysis_in['scenario_id'],
+                                          "testruns.id": analysis_in['testrun_id']},
+                                   data={"testruns.$.measure_targets": {'type': _type}})
 
         RedisClient.publish('command', set_redis_pub_msg(msg="start_analysis"))
-
     except Exception as e:
         logger.error(traceback.format_exc())
         raise HTTPException(status_code=500, detail=traceback.format_exc())
