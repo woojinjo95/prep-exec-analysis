f32fe9507fc0227d7cf97e3cb52f42f83901f0a6
정보미 <jeongbomi@nextlab.co.kr>
2023-09-15 18:46:41 +0900

    PointChart, RangeChart를 사용하는 차트 - 툴팁으로 선택된 데이터 강조 표시

--- frontend/src/page/AnalysisPage/components/TimelineSection/components/MonkeyTestChart.tsx
+++ frontend/src/page/AnalysisPage/components/TimelineSection/components/MonkeyTestChart.tsx
@@ -75,7 +75,7 @@ const MonkeyTestChart: React.FC<MonkeyTestChartProps> = ({
       {!!posX && (
         <div
           ref={wrapperRef}
-          className="absolute top-0 h-full w-1 bg-white opacity-30 z-[5]"
+          className="absolute top-0 h-full w-1 bg-white/30 z-[5]"
           style={{
             transform: `translateX(${posX - 2}px)`,
           }}
