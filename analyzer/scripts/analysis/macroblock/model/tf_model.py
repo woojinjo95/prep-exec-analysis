6ec32367b96e8c20fb8e3b66f4cae6a72bc4eed7
김준한 <kjune1236@gmail.com>
2023-09-18 13:20:27 +0900

    fix download and load model for tflite

--- analyzer/scripts/analysis/macroblock/model/tf_model.py
+++ analyzer/scripts/analysis/macroblock/model/tf_model.py
@@ -13,32 +13,24 @@ logger = logging.getLogger('main')
 
 
 class TensorflowModel:
-    def __init__(self, model_dir_url: str, model_output_dir: str):
-        """_summary_
-        Args:
-            model_dir_url (str): model directory path
-            model_output_dir (str): model save directory
-        """
+    def __init__(self, model_url: str, model_output_dir: str):
         self.output_dir = model_output_dir
 
         # load model
-        logger.info(f'model url: {model_dir_url}')
-        model_dir = self.download_model(model_dir_url)
-        self.model = self.load_model(model_dir)
+        logger.info(f'model url: {model_url}')
+        model_path = self.download_model(model_url)
+        self.model = self.load_model(model_path)
 
-        self.input_shape = self.model.input_shape[1:]
-        logger.info(f'input shape: {self.input_shape}')
+        self.input_shape = self.get_input_shape()
 
-    
-    def download_model(self, model_dir_url):
-        model_name = re.search('folders/(.*)\?usp', model_dir_url).group(1)
-        model_dir = os.path.join(self.output_dir, model_name)
+    def download_model(self, model_url: str) -> str:
+        model_name = re.search('file/d/(.*)/view?', model_url).group(1)
+        model_path = os.path.join(self.output_dir, model_name) + '.tflite'
 
-        if not os.path.exists(model_dir):
+        if not os.path.exists(model_path):
             os.makedirs(self.output_dir, exist_ok=True)
-            os.makedirs(model_dir, exist_ok=True)
             try:
-                gdown.download_folder(url=model_dir_url, output=model_dir)
+                gdown.download(url=model_url, output=model_path)
                 time.sleep(10)
                 logger.info('Model Download Complete.')
             except Exception:
@@ -46,21 +38,46 @@ class TensorflowModel:
                 raise Exception('Model Download Failed.')
         else:
             logger.info('Model Already Exists.')
-        return model_dir
+        return model_path
 
-    def load_model(self, model_dir: str):
-        model = tf.keras.models.load_model(model_dir)
-        return model
+    def load_model(self, model_path: str) -> tf.lite.Interpreter:
+        interpreter = tf.lite.Interpreter(model_path=model_path)
+        interpreter.allocate_tensors()
+        return interpreter
 
-    def predict_with_batch(self, batch):
-        result = self.model.predict_on_batch(batch)
-        return result
+    def predict_with_batch(self, batch) -> np.ndarray:
+        # Get input and output details
+        input_details = self.model.get_input_details()
+        output_details = self.model.get_output_details()
 
+        # Check if the interpreter supports batching.
+        if input_details[0]['shape'][0] == 1:
+            results = []
+            for sample in batch:
+                sample = np.expand_dims(sample, axis=0).astype(input_details[0]['dtype'])
+                self.model.set_tensor(input_details[0]['index'], sample)
+                self.model.invoke()
+                output_data = self.model.get_tensor(output_details[0]['index'])
+                results.append(output_data)
+            return np.vstack(results)
+        else:
+            # In this case, your tflite model is already configured to accept batches.
+            batch = np.array(batch).astype(input_details[0]['dtype'])
+            self.model.set_tensor(input_details[0]['index'], batch)
+            self.model.invoke()
+            output_data = self.model.get_tensor(output_details[0]['index'])
+            return output_data
+
+    def get_input_shape(self) -> tuple:
+        input_details = self.model.get_input_details()
+        input_shape = input_details[0]['shape'][1:]
+        logger.info(f'input shape: {self.input_shape}')
+        return input_shape
 
 class MacroblockModel(TensorflowModel):
 
-    def __init__(self, model_dir_url: str, model_output_dir: str):
-        super().__init__(model_dir_url=model_dir_url, model_output_dir=model_output_dir)
+    def __init__(self, model_url: str, model_output_dir: str):
+        super().__init__(model_url=model_url, model_output_dir=model_output_dir)
 
     # preprocess for macroblock
     def preprocess(self, image):
