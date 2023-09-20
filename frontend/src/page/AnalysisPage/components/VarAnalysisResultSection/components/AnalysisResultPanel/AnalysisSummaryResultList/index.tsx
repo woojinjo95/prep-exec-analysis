c6bc1cb40ed1794f1475c35a14b3d359e8909fd4
정보미 <jeongbomi@nextlab.co.kr>
2023-09-20 15:09:37 +0900

    분석결과 요약 아이템 - 스켈레톤 로딩 적용

--- frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/components/AnalysisResultPanel/AnalysisSummaryResultList/index.tsx
+++ frontend/src/page/AnalysisPage/components/VarAnalysisResultSection/components/AnalysisResultPanel/AnalysisSummaryResultList/index.tsx
@@ -28,7 +28,23 @@ interface AnalysisSummaryResultListProps {
 const AnalysisSummaryResultList: React.FC<AnalysisSummaryResultListProps> = ({ summary, startTime, endTime }) => {
   const [rawDataModalType, setRawDataModalType] = useState<keyof AnalysisResultSummary | null>(null)
 
-  if (!summary || !startTime || !endTime) return null
+  if (!summary || !startTime || !endTime) {
+    return (
+      <div className="flex flex-col gap-y-1">
+        {range(3).map((num) => (
+          <Skeleton key={`analysis-summary-result-list-skeleton-${num}`} colorScheme="dark" className="rounded-lg">
+            <Accordion
+              header={
+                <div className="flex">
+                  <Text size="sm">loading</Text>
+                </div>
+              }
+            />
+          </Skeleton>
+        ))}
+      </div>
+    )
+  }
   return (
     <div className="overflow-y-auto flex flex-col gap-y-1">
       {summary.freeze && <FreezeSummaryItem freeze={summary.freeze} setRawDataModalType={setRawDataModalType} />}
